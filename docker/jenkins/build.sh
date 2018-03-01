6eaa3f4a66af2ae2f39784f1c4716a3068aea9ce
Damien L-G <dalg24@gmail.com>
2018-03-01 16:37:26 -0500

    [fixup] Remove CMake arg to disable in Jenkins CI build script as suggested in review

--- docker/jenkins/build.sh
+++ docker/jenkins/build.sh
@@ -8,7 +8,6 @@ cmake \
   -D CMAKE_CXX_FLAGS="-Wall -Wextra" \
   -D CMAKE_CXX_COMPILER=g++ \
   -D PYTHON_EXECUTABLE=/usr/bin/python3 \
-  -D Tasmanian_ENABLE_MATLAB=OFF \
   ..
 make -j${NPROC}
 ctest -j${NPROC} --no-compress-output -T Test
