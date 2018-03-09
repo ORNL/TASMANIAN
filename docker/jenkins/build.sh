d858ba5a43d19b4abc4e8b495b01550ecb949e24
Damien L-G <dalg24@gmail.com>
2018-03-09 17:45:20 -0500

    Update docker/jenkins/build.sh for multijob configuration on jenkins-ci

--- docker/jenkins/build.sh
+++ docker/jenkins/build.sh
@@ -4,10 +4,23 @@ set -e
 
 [ -d build ] && rm -rf build
 mkdir build && cd build
-cmake \
-  -D CMAKE_CXX_FLAGS="-Wall -Wextra" \
-  -D CMAKE_CXX_COMPILER=g++ \
-  -D PYTHON_EXECUTABLE=/usr/bin/python3 \
-  ..
+if [ "${BUILD_TYPE}" == "clang50-python3" ]
+then
+    cmake \
+      -D CMAKE_CXX_FLAGS="-Wall -Wextra" \
+      -D CMAKE_CXX_COMPILER=clang++ \
+      -D PYTHON_EXECUTABLE=/usr/bin/python3 \
+      ..
+elif [ "${BUILD_TYPE}" == "gcc73-python2" ]
+then
+    cmake \
+      -D CMAKE_CXX_FLAGS="-Wall -Wextra" \
+      -D CMAKE_CXX_COMPILER=g++ \
+      -D PYTHON_EXECUTABLE=/usr/bin/python2 \
+      ..
+else
+    echo "Unknown BUILD_TYPE"
+    exit 1
+fi
 make -j${NPROC}
 ctest -j${NPROC} --no-compress-output -T Test
