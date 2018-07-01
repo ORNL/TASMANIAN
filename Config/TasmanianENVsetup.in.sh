87807fbc7666fba4be7003c449bd68b4f32695ca
Miroslav Stoyanov <stoyanovmk@ornl.gov>
2018-06-30 20:20:08 -0400

    add PYTHONPATH to the environmental variables source script

--- Config/TasmanianENVsetup.in.sh
+++ Config/TasmanianENVsetup.in.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 # source this file to execute tasgrid and tasdream after install
+# also sets the python path
 export PATH=$PATH:@CMAKE_INSTALL_PREFIX@/bin/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:@CMAKE_INSTALL_PREFIX@/lib/
+if [[ "@Tasmanian_ENABLE_PYTHON@" == "ON" ]]; then
+    export PYTHONPATH=$PYTHONPATH:@CMAKE_INSTALL_PREFIX@/share/Tasmanian/python/
+fi
