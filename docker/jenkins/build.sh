fedf8385952a81f9d5d3d36f0a142cdee406832a
mkstoyanov <stoyanovmk@ornl.gov>
2018-07-20 17:47:17 -0400

    removed obsolete option

--- docker/jenkins/build.sh
+++ docker/jenkins/build.sh
@@ -45,7 +45,6 @@ then
       -D Tasmanian_ENABLE_BLAS=ON \
       -D Tasmanian_ENABLE_CUDA=ON \
       -D Tasmanian_ENABLE_PYTHON=ON \
-      -D Tasmanian_ENABLE_MATLAB=OFF \
       -D Tasmanian_ENABLE_MPI=OFF \
       -D Tasmanian_ENABLE_FORTRAN=OFF \
       -D Tasmanian_TESTS_OMP_NUM_THREADS=4 \
