b02c2faafae6c43ce9ddda1797cd5fcfb4b7ff11
Viktor Reshniak <revitmt@gmail.com>
2018-07-25 01:50:50 -0400

    fixed bug in tsgghc_()
    complex coefficients need twice as many storage

--- InterfaceFortran/tsgC2Fortran.cpp
+++ InterfaceFortran/tsgC2Fortran.cpp
@@ -184,7 +184,8 @@ void tsgehz_(int *id, const double *x, int *num_x, int *num_nz){
     *num_nz = _tsg_grid_list[*id]->evaluateSparseHierarchicalFunctionsGetNZ(x, *num_x);}
 void tsgghc_(int *id, double *c){
     const double *cc = _tsg_grid_list[*id]->getHierarchicalCoefficients();
-    std::copy(cc, cc + _tsg_grid_list[*id]->getNumPoints() * _tsg_grid_list[*id]->getNumOutputs(), c);
+    _tsg_grid_list[*id]->isFourier() ? std::copy(cc, cc + 2 * _tsg_grid_list[*id]->getNumPoints() * _tsg_grid_list[*id]->getNumOutputs(), c)
+                                     : std::copy(cc, cc + _tsg_grid_list[*id]->getNumPoints() * _tsg_grid_list[*id]->getNumOutputs(), c);
 }
 void tsggchc_(int *id, struct dcmplx *c){
     const double *cc = _tsg_grid_list[*id]->getHierarchicalCoefficients();
