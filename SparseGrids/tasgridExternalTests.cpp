028a2d9b794b608abcf531952d4c345764feda60
mkstoyanov <stoyanovmk@ornl.gov>
2018-07-31 17:43:35 -0400

    used to keep for reference, not needed anymore

--- SparseGrids/tasgridExternalTests.cpp
+++ SparseGrids/tasgridExternalTests.cpp
@@ -107,28 +107,6 @@ TestResults ExternalTester::getError(const BaseFunction *f, TasGrid::TasmanianSp
             for(int k=0; k<num_outputs; k++) r[k] += weights[i] * y[k];
         }
 
-        //#pragma omp parallel
-        //{
-        //    double *y_local = new double[num_outputs];
-        //    double *r_local = new double[num_outputs];  std::fill(r_local, r_local + num_outputs, 0.0);
-        //    #pragma omp for
-        //    for(int i=0; i<num_points; i++){
-        //        f->eval(&(points[i*num_dimensions]), y_local);
-        //        for(int j=0; j<num_outputs; j++){
-        //            r_local[j] += weights[i] * y_local[j];
-        //        }
-        //    }
-        //
-        //    #pragma omp critical
-        //    {
-        //        for(int j=0; j<num_outputs; j++){
-        //            r[j] += r_local[j];
-        //        }
-        //    }
-        //    delete[] y_local;
-        //    delete[] r_local;
-        //}
-
         double err = 0.0;
         if (type == type_integration){
             f->getIntegral(y.data());
