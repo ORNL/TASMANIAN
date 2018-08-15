32772edc8962d92c571bad8c18c5f13e9e7da4d9
mkstoyanov <stoyanovmk@ornl.gov>
2018-08-15 15:55:44 -0400

    no overflow in get points

--- SparseGrids/tsgGridSequence.cpp
+++ SparseGrids/tsgGridSequence.cpp
@@ -297,21 +297,27 @@ int GridSequence::getNumPoints() const{ return ((points == 0) ? getNumNeeded() :
 
 void GridSequence::getLoadedPoints(double *x) const{
     int num_points = points->getNumIndexes();
-    #pragma omp parallel for
+    Data2D<double> split;
+    split.load(num_dimensions, num_points, x);
+    #pragma omp parallel for schedule(static)
     for(int i=0; i<num_points; i++){
         const int *p = points->getIndex(i);
+        double *xx = split.getStrip(i);
         for(int j=0; j<num_dimensions; j++){
-            x[i*num_dimensions + j] = nodes[p[j]];
+            xx[j] = nodes[p[j]];
         }
     }
 }
 void GridSequence::getNeededPoints(double *x) const{
     int num_points = needed->getNumIndexes();
-    #pragma omp parallel for
+    Data2D<double> split;
+    split.load(num_dimensions, num_points, x);
+    #pragma omp parallel for schedule(static)
     for(int i=0; i<num_points; i++){
         const int *p = needed->getIndex(i);
+        double *xx = split.getStrip(i);
         for(int j=0; j<num_dimensions; j++){
-            x[i*num_dimensions + j] = nodes[p[j]];
+            xx[j] = nodes[p[j]];
         }
     }
 }
