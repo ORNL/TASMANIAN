4c37aec745812a121dc673aa3fcf3c63720c1144
mkstoyanov <stoyanovmk@ornl.gov>
2018-09-18 15:53:06 -0400

    add test for the hard-coded sequence values vs the actual computed ones

--- SparseGrids/tasgridExternalTests.cpp
+++ SparseGrids/tasgridExternalTests.cpp
@@ -387,6 +387,32 @@ bool ExternalTester::performGLobalTest(TasGrid::TypeOneDRule rule) const{
         }else{
             cout << setw(wfirst) << "Rule" << setw(wsecond) << TasGrid::OneDimensionalMeta::getIORuleString(oned) << setw(wthird) << "FAIL" << endl;  pass = false;
         }}
+        // test the hard-coded sequence values vs the optimizer
+        if (rule == rule_minlebesgue){
+            int n = 22;
+            GreedySequences greed;
+            std::vector<double> minleb;
+            greed.getMinLebesgueNodes(n, minleb);
+
+            MinLebesgue g(n, minleb.data());
+            OptimizerResult R = Optimizer::argMaxGlobal(&g);
+            if (fabs(R.xmax - greed.getMinLebesgueStored(n)) > 1.E-8){
+                pass = false;
+                cout << "ERROR: mismatch in stored vs computed nodes for rule_minlebesgue rule" << endl;
+            }
+        }else if (rule == rule_mindelta){
+            int n = 22;
+            GreedySequences greed;
+            std::vector<double> mindel;
+            greed.getMinDeltaNodes(n, mindel);
+
+            MinDelta d(n, mindel.data());
+            OptimizerResult R = Optimizer::argMaxGlobal(&d);
+            if (fabs(R.xmax - greed.getMinDeltaStored(n)) > 1.E-9){ // this seems large, double-check
+                pass = false;
+                cout << "ERROR: mismatch in stored vs computed nodes for rule_mindelta rule" << endl;
+            }
+        }
     }else if ((rule == TasGrid::rule_gausslegendre) || (rule == TasGrid::rule_gausslegendreodd)){
         { TasGrid::TypeOneDRule oned = rule;
         const int depths1[3] = { 20, 36, 38 };
