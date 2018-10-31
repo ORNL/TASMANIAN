1f011c34dea573791e3a93693c75dd0fa85db89f
mkstoyanov <stoyanovmk@ornl.gov>
2018-10-31 16:52:49 -0400

    convert empty class to namespace

--- SparseGrids/tsgHiddenExternals.hpp
+++ SparseGrids/tsgHiddenExternals.hpp
@@ -31,6 +31,8 @@
 #ifndef __TASMANIAN_SPARSE_GRID_HIDDEN_INTERNALS_HPP
 #define __TASMANIAN_SPARSE_GRID_HIDDEN_INTERNALS_HPP
 
+#include "TasmanianConfig.hpp"
+
 namespace TasGrid{
 
 // interfaces to external libraries, for convenience purposes mostly
@@ -46,71 +48,70 @@ extern "C" void dtrsm_(const char *side, const char *uplo, const char* transa, c
 #endif // Tasmanian_ENABLE_BLAS
 
 
-class TasBLAS{
-public:
+namespace TasBLAS{
 #ifdef Tasmanian_ENABLE_BLAS
     // can't find BLAS implementation that is generally reliable
     // if dgemv_ is called within OpenMP region with large matrices, it returns wrong result!
     // Level 1
-    inline static void daxpy(int N, double alpha, const double x[], double y[]){
+    inline void daxpy(int N, double alpha, const double x[], double y[]){
         int ione = 1;
         daxpy_(&N, &alpha, x, &ione, y, &ione);
     }
-    inline static double ddot(int N, const double x[], const double y[]){
+    inline double ddot(int N, const double x[], const double y[]){
         int ione = 1;
         return ddot_(&N, x, &ione, y, &ione);
     }
-    inline static double ddot(int N, const double x[]){
+    inline double ddot(int N, const double x[]){
         int ione = 1;
         double nrm = dnrm2_(&N, x, &ione);
         return nrm * nrm;
     }
     // Level 2
-    inline static void dgemv(int M, int N, const double A[], const double x[], double y[], double alpha = 1.0, double beta = 0.0){ // y = A*x, A is M by N
+    inline void dgemv(int M, int N, const double A[], const double x[], double y[], double alpha = 1.0, double beta = 0.0){ // y = A*x, A is M by N
         char charN = 'N'; int blas_one = 1;
         dgemv_(&charN, &M, &N, &alpha, A, &M, x, &blas_one, &beta, y, &blas_one);
     }
-    inline static void dgemtv(int M, int N, const double A[], const double x[], double y[]){ // y = A^T*x, A is M by N
+    inline void dgemtv(int M, int N, const double A[], const double x[], double y[]){ // y = A^T*x, A is M by N
         char charT = 'T'; int blas_one = 1; double alpha = 1.0, beta = 0.0;
         dgemv_(&charT, &M, &N, &alpha, A, &M, x, &blas_one, &beta, y, &blas_one);
     }
-    inline static void dtrsv_UTN(int N, const double A[], double B[]){ // solve A = U^{-T} B (A comes from cholUTU)
+    inline void dtrsv_UTN(int N, const double A[], double B[]){ // solve A = U^{-T} B (A comes from cholUTU)
         char charU = 'U', charT = 'T', charN = 'N'; int ione = 1;
         dtrsv_(&charU, &charT, &charN, &N, A, &ione, B, &ione);
     }
-    inline static void dtrsv_UNN(int N, const double A[], double B[]){ // solve A = U^{-1} B (A comes from cholUTU)
+    inline void dtrsv_UNN(int N, const double A[], double B[]){ // solve A = U^{-1} B (A comes from cholUTU)
         char charU = 'U', charN = 'N'; int ione = 1;
         dtrsv_(&charU, &charN, &charN, &N, A, &ione, B, &ione);
     }
     // Level 3
-    inline static void dgemm(int M, int N, int K, double alpha, const double A[], const double B[], double beta, double C[]){
+    inline void dgemm(int M, int N, int K, double alpha, const double A[], const double B[], double beta, double C[]){
         char charN = 'N';
         dgemm_(&charN, &charN, &M, &N, &K, &alpha, A, &M, B, &K, &beta, C, &M);
     }
-    inline static void dtrsm_LUTN(int M, int N, const double A[], const double B[]){ // solve A = U^{-T} B (A comes from cholUTU)
+    inline void dtrsm_LUTN(int M, int N, const double A[], const double B[]){ // solve A = U^{-T} B (A comes from cholUTU)
         char charL = 'L', charU = 'U', charT = 'T', charN = 'N'; double alpha = 1.0; int ione = 1;
         dtrsm_(&charL, &charU, &charT, &charN, &M, &N, &alpha, A, &ione, B, &ione);
     }
-    inline static void dtrsm_LUNN(int M, int N, const double A[], const double B[]){ // solve A = U^{-1} B (A comes from cholUTU)
+    inline void dtrsm_LUNN(int M, int N, const double A[], const double B[]){ // solve A = U^{-1} B (A comes from cholUTU)
         char charL = 'L', charU = 'U', charN = 'N'; double alpha = 1.0; int ione = 1;
         dtrsm_(&charL, &charU, &charN, &charN, &M, &N, &alpha, A, &ione, B, &ione);
     }
 #else
     // non optimal BLAS subroutines, in case there is no BLAS available
-    inline static void daxpy(int N, double alpha, const double x[], double y[]){
+    inline void daxpy(int N, double alpha, const double x[], double y[]){
         for(int i=0; i<N; i++) y[i] += alpha * x[i];
     }
-    inline static double ddot(int N, const double x[], const double y[]){
+    inline double ddot(int N, const double x[], const double y[]){
         double sum = 0.0;
         for(int i=0; i<N; i++) sum += x[i] * y[i];
         return sum;
     }
-    inline static double ddot(int N, const double x[]){
+    inline double ddot(int N, const double x[]){
         double sum = 0.0;
         for(int i=0; i<N; i++) sum += x[i] * x[i];
         return sum;
     }
-    inline static void dgemm(int M, int N, int K, double alpha, const double A[], const double B[], double beta, double C[]){
+    inline void dgemm(int M, int N, int K, double alpha, const double A[], const double B[], double beta, double C[]){
         for(int j=0; j<N; j++){
             for(int i=0; i<M; i++){
                 double sum = 0.0;
@@ -121,32 +122,32 @@ public:
             }
         }
     }
-    inline static void dgemv(int M, int N, const double A[], const double x[], double y[]){ // y = A*x, A is M by N
+    inline void dgemv(int M, int N, const double A[], const double x[], double y[]){ // y = A*x, A is M by N
         for(int i=0; i<M; i++){
             y[i] = A[i] * x[0];
             for(int j=1; j<N; j++) y[i] += A[j*M + i] * x[j];
         }
     }
-    inline static void dgemtv(int M, int N, const double A[], const double x[], double y[]){ // y = A^T*x, A^T is M by N
+    inline void dgemtv(int M, int N, const double A[], const double x[], double y[]){ // y = A^T*x, A^T is M by N
         for(int i=0; i<M; i++){
             y[i] = A[i*M] * x[0];
             for(int j=1; j<N; j++) y[i] += A[i*M + j] * x[j];
         }
     }
-    inline static void dtrsv_UTN(int N, const double A[], double B[]){ // solve A = U^{-T} B (A comes from cholUTU)
+    inline void dtrsv_UTN(int N, const double A[], double B[]){ // solve A = U^{-T} B (A comes from cholUTU)
         B[0] /= A[0];
         for(int i=1; i<N; i++){
             for(int j=0; j<i; j++) B[i] -= A[i*N + j] * B[i];
             B[i] /= A[i*N + i];
         }
     }
-    inline static void dtrsv_UNN(int N, const double A[], double B[]){ // solve A = U^{-1} B (A comes from cholUTU)
+    inline void dtrsv_UNN(int N, const double A[], double B[]){ // solve A = U^{-1} B (A comes from cholUTU)
         for(int i=N-1; i>=0; i--){
             B[i] /= A[(i+1)*N-1];
             for(int j=i-1; j>=0; j--) B[j] -= A[i*N + j] * B[i];
         }
     }
-    inline static void dtrsm_LUTN(int M, int N, const double A[], double B[]){ // solve A = U^{-T} B (A comes from cholUTU)
+    inline void dtrsm_LUTN(int M, int N, const double A[], double B[]){ // solve A = U^{-T} B (A comes from cholUTU)
         //char charL = 'L', charL = 'U', charL = 'T', charN = 'N'; double alpha = 1.0; int ione = 1;
         //dtrsm_(&charL, &charU, &charT, &charN, &M, &N, &alpha, A, &ione, B, &ione);
         for(int b=0; b<N; b++){ // for each column of B
@@ -157,7 +158,7 @@ public:
             }
         }
     }
-    inline static void dtrsm_LUNN(int M, int N, const double A[], double B[]){ // solve A = U^{-1} B (A comes from cholUTU)
+    inline void dtrsm_LUNN(int M, int N, const double A[], double B[]){ // solve A = U^{-1} B (A comes from cholUTU)
         //char charL = 'L', charL = 'U', charN = 'N'; double alpha = 1.0; int ione = 1;
         //dtrsm_(&charL, &charU, &charN, &charN, &M, &N, &alpha, A, &ione, B, &ione);
         for(int b=0; b<N; b++){ // for each column of B
@@ -170,7 +171,7 @@ public:
 #endif // Tasmanian_ENABLE_BLAS
 
     // used to compute anisotropic weights (at most 2 * dims X 2 * dims, not worth adding lapack dependence for such small matrix)
-    inline static void cholUTU(int N, double A[], int lda = -1){
+    inline void cholUTU(int N, double A[], int lda = -1){
         if (lda == -1) lda = N;
         for(int i=0; i<N; i++){
             for(int k=0; k<i; k++) A[i*lda + i] -= A[i*lda +k]*A[i*lda +k];
@@ -182,9 +183,8 @@ public:
         }
     }
 
-
-    inline static void setzero(int N, double *A){ std::fill(A, A + N, 0.0); } // fill A with N zeros
-    inline static void setzero(int N, int *A){ std::fill(A, A + N, 0); } // fill A with N zeros
+    inline void setzero(int N, double *A){ std::fill(A, A + N, 0.0); } // fill A with N zeros
+    inline void setzero(int N, int *A){ std::fill(A, A + N, 0); } // fill A with N zeros
 
 };
 
