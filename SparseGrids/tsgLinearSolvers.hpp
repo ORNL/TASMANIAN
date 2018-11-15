e44deeff4ebd561fd56ddc3acd86843f521dd22e
Miroslav Stoyanov <stoyanovmk@ornl.gov>
2018-11-14 21:25:23 -0500

    document the linear solvers

--- SparseGrids/tsgLinearSolvers.hpp
+++ SparseGrids/tsgLinearSolvers.hpp
@@ -39,23 +39,74 @@
 
 #include "tsgEnumerates.hpp"
 
+//! \internal
+//! \file tsgLinearSolvers.hpp
+//! \brief Linear solvers.
+//! \author Miroslav Stoyanov
+//! \ingroup TasmanianLinearSolvers
+//!
+//! Many sparse grids methods rely on various linear solvers, sparse iterative, eigenvalue,
+//! fast-fourier-transform, and least-squares. The solvers are tuned for the specific
+//! problems and can perform as good (or even better) than general third-party methods.
+//! In addition, this reduces the external dependencies.
+
+//! \internal
+//! \defgroup TasmanianLinearSolvers Linear solvers.
+//!
+//! \par Linear Solvers
+//! Many sparse grids methods rely on various linear solvers, sparse iterative, eigenvalue,
+//! fast-fourier-transform, and least-squares. The solvers are tuned for the specific
+//! problems and can perform as good (or even better) than general third-party methods.
+//! In addition, this reduces the external dependencies.
+
 namespace TasGrid{
 
+//! \internal
+//! \brief Methods for dense linear algebra.
+//! \ingroup TasmanianLinearSolvers
 namespace TasmanianDenseSolver{
+    //! \internal
+    //! \brief Least squares solver, used to infer anisotropic coefficients and thus rarely exceeds 10 - 20.
+    //! \ingroup TasmanianLinearSolvers
+
+    //! Matrix \b A is \b n by \b m (with \b n much larger), vector \b b has length \b n,
+    //! the result is given in \b x (length \b m). The regularized term \b reg is added to the diagonal of
+    //! the product `transpose(A) * A` to stabilize the Cholesky decomposition.
     void solveLeastSquares(int n, int m, const double A[], const double b[], double reg, double *x);
 }
 
+//! \internal
+//! \brief Methods for tridiagonal eigenvalue problems.
+//! \ingroup TasmanianLinearSolvers
 namespace TasmanianTridiagonalSolver{
+    //! \internal
+    //! \brief Methods for tridiagonal eigenvalue problems, used to compute nodes and weights for Gaussian rules.
+    //! \ingroup TasmanianLinearSolvers
     void decompose(int n, std::vector<double> &d, std::vector<double> &e, std::vector<double> &z);
 }
 
+//! \internal
+//! \brief Methods for Fast-Fourier-Transform.
+//! \ingroup TasmanianLinearSolvers
 namespace TasmanianFourierTransform{
+    //! \internal
+    //! \brief Transfrom the data for a multi-dimensional tensor.
+    //! \ingroup TasmanianLinearSolvers
+
+    //! The \b num_points vector defines the number of points used by the tensor in different directions.
+    //! The \b data holds the values for each point in the tensor, each point has a vector of values
+    //! with size equal to the number of model outputs.
+    //!
+    //! The data is split into one-dimensional \a lines and each is tackled with \b fast_fourier_transform1D()
     void fast_fourier_transform(std::vector<std::vector<std::complex<double>>> &data, std::vector<int> &num_points);
-    // num_points describes the dimension and number of points in the tensor
-    // data holds the corresponding strips with size num_outputs
+
+    //! \internal
+    //! \brief Perform one dimensional fast-fourier-transform (using radix-3).
+    //! \ingroup TasmanianLinearSolvers
+
+    //! Consider the line which is a subset of \b data defined by \b indexes and perform the radix-3 fast-fourier-transform.
+    //! Called from \b fast_fourier_transform().
     void fast_fourier_transform1D(std::vector<std::vector<std::complex<double>>> &data, std::vector<int> &indexes);
-    // indexes lists the indexes that play a role in this strip, i.e., 1D transform
-    // data holds the corresponding strips with size num_outputs
 }
 
 }
@@ -64,21 +115,30 @@ namespace TasmanianFourierTransform{
 // tsgEnumerates.hpp is needed by TasSparse for the hardcoded constants, i.e., TSG_DROP_TOL
 namespace TasSparse{
 
-// Make a new matrix class that would store the ILU preconditioner and solve either the regular or adjoined problem all in one class
+//! \internal
+//! \brief Used to manipulate the wavelet values and solve for the wavelet coefficients.
+//! \ingroup TasmanianLinearSolvers
 class SparseMatrix{
 public:
+    //! \brief Default constructor, create an empty matrix.
     SparseMatrix();
+    //! \brief Default destructor.
     ~SparseMatrix();
 
+    //! \brief Load the sparse matrix in row-compressed form.
     void load(const std::vector<int> &lpntr, const std::vector<std::vector<int>> &lindx, const std::vector<std::vector<double>> &lvals);
 
+    //! \brief Return the number of rows in the matrix.
     int getNumRows() const;
 
+    //! \brief Solve `op(A) x = b` where `op` is either identity (find the coefficients) or transpose (find the interpolation weights).
 	void solve(const double b[], double x[], bool transposed = false) const;
 
 protected:
+    //! \brief Clear the internal data structures (maybe not needed?)
     void clear();
 
+    //! \brief Compute the incomplete lower-upper decomposition of the matrix (zero extra fill).
     void computeILU();
 
 private:
