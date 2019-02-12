c56bda876a609a1a5e57b70cccd0c9e6ebce83c2
mkstoyanov <stoyanovmk@ornl.gov>
2019-02-12 13:24:58 -0500

    remove obsolete code

--- SparseGrids/tsgGridFourier.hpp
+++ SparseGrids/tsgGridFourier.hpp
@@ -57,12 +57,6 @@ public:
     template<bool useAscii> void write(std::ostream &os) const;
     template<bool useAscii> void read(std::istream &is);
 
-    void write(std::ofstream &ofs) const;
-    void read(std::ifstream &ifs);
-
-    void writeBinary(std::ofstream &ofs) const;
-    void readBinary(std::ifstream &ifs);
-
     void makeGrid(int cnum_dimensions, int cnum_outputs, int depth, TypeDepth type, const std::vector<int> &anisotropic_weights, const std::vector<int> &level_limits);
     void copyGrid(const GridFourier *fourier);
 
