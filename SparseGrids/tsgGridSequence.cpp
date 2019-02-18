cc7c3cb35125c83919d2d6b6e3a59c39cb5d59d6
mkstoyanov <stoyanovmk@ornl.gov>
2019-02-18 17:07:51 -0500

    blas for sequence grids

--- SparseGrids/tsgGridSequence.cpp
+++ SparseGrids/tsgGridSequence.cpp
@@ -472,6 +472,14 @@ void GridSequence::evaluateBatchCPUblas(const double x[], int num_x, double y[])
     TasBLAS::dgemm(num_outputs, num_x, num_points, 1.0, surpluses.data(), weights.getStrip(0), 0.0, y);
 }
 void GridSequence::evaluateBlas(const double x[], int num_x, double y[]) const{
+    int num_points = points.getNumIndexes();
+    Data2D<double> weights; weights.resize(num_points, num_x);
+    if (num_x > 1){
+        evaluateHierarchicalFunctions(x, num_x, weights.getStrip(0));
+    }else{ // workaround small OpenMP penalty
+        evalHierarchicalFunctions(x, weights.getStrip(0));
+    }
+    TasBLAS::denseMultiply(num_outputs, num_x, num_points, 1.0, surpluses.data(), weights.getStrip(0), 0.0, y);
 }
 #endif // Tasmanian_ENABLE_BLAS
 
