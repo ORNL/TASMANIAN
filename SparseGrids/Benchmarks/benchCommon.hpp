736f854814d82e7a29d5fb68acb3d059d8383f8b
Miroslav Stoyanov <stoyanovmk@ornl.gov>
2019-04-09 21:12:30 -0400

    parse arguments and check for help

--- SparseGrids/Benchmarks/benchCommon.hpp
+++ SparseGrids/Benchmarks/benchCommon.hpp
@@ -11,4 +11,26 @@
 using std::cout;
 using std::endl;
 
+// skips the first argument, then goes over the others
+std::vector<std::string> stringArgs(int argc, char** argv){
+    std::vector<std::string> args;
+    for(int i = 1; i < argc; i++)
+        args.push_back(std::string(argv[i]));
+    return args;
+}
+
+bool hasHelp(std::string const &arg){
+    std::string lower(arg.size(), ' ');
+    std::transform(arg.begin(), arg.end(), lower.begin(),
+        [](char c)->char{
+            return static_cast<char>(std::tolower(static_cast<int>(c)));
+        });
+
+    auto pos = lower.find("help");
+    if ((pos < lower.size()) && (pos  + 4 <= lower.size()))
+        return (lower.substr(pos, pos + 4).compare("help") == 0);
+
+    return false;
+}
+
 #endif
--- SparseGrids/Benchmarks/bench_main.cpp
+++ SparseGrids/Benchmarks/bench_main.cpp
@@ -4,10 +4,15 @@ void printHelp();
 
 int main(int argc, char** argv){
 
-    cout << " Phruuuuphrrr " << endl; // this is the sound that the Tasmanian devil makes
+    //cout << " Phruuuuphrrr " << endl; // this is the sound that the Tasmanian devil makes
+
+    std::vector<std::string> args = stringArgs(argc, argv);
+
+    if (args.empty() || hasHelp(args.front())) printHelp();
 
     return 0;
 }
 
 void printHelp(){
+    cout << "help\n";
 }
