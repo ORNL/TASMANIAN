38cd8931b2386e7a17832eaea2a836e47109451d
mkstoyanov <stoyanovmk@ornl.gov>
2019-08-08 16:17:31 -0400

    updated file structure for example 4

---
+++ SparseGrids/Examples/example_sparse_grids_04.cpp
@@ -0,0 +1,41 @@
+
+#include "Tasmanian.hpp"
+
+using namespace std;
+
+/*!
+ * \file example_sparse_grids_04.cpp
+ * \brief Examples for the Tasmanian Sparse Grid module.
+ * \author Miroslav Stoyanov
+ * \ingroup TasmanianSGExamples
+ *
+ * Tasmanian Sparse Grids Example 4.
+ */
+
+/*!
+ * \ingroup TasmanianSGExamples
+ * \addtogroup TasmanianSGExamples4 Tasmanian Sparse Grids module, example 4
+ *
+ * \par Example 4
+ *
+ */
+
+/*!
+ * \ingroup TasmanianSGExamples4
+ * \brief Sparse Grids Example 4:
+ *
+ * \snippet SparseGrids/Examples/example_sparse_grids_04.cpp SG_Example_04 example
+ */
+void sparse_grids_example_04(){
+#ifndef __TASMANIAN_DOXYGEN_SKIP
+//! [SG_Example_04 example]
+#endif
+
+    cout << "\n---------------------------------------------------------------------------------------------------\n";
+    cout << std::scientific; cout.precision(4);
+    cout << "Example 4:\n";
+
+#ifndef __TASMANIAN_DOXYGEN_SKIP
+//! [SG_Example_04 example]
+#endif
+}
