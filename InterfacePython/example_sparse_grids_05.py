7f8a6548b8ac25f6a2e9cd7fc6ad75f0a50e3fdd
mkstoyanov <stoyanovmk@ornl.gov>
2019-08-14 17:26:20 -0400

    added python equivalent to example 5

--- InterfacePython/example_sparse_grids_05.py
+++ InterfacePython/example_sparse_grids_05.py
@@ -31,9 +31,98 @@
 import numpy as np
 import Tasmanian
 
+import matplotlib.pyplot as pyplot
+
 def example_05():
-    Tasmanian.loadNeededPoints()
-    pass
+
+    print("\n---------------------------------------------------------------------------------------------------\n")
+    print("Example 5: interpolate f(x,y) = exp(-x^2) * cos(y), using leja rule")
+    print("           employ adaptive refinement to increase accuracy per samples")
+
+    iNumInputs = 2
+    iNumOutputs = 1
+    def model(aX):
+        return np.ones((1,)) * np.exp(-aX[0] * aX[0]) * np.cos(aX[1])
+
+    iTestGridSize = 33
+    dx = np.linspace(-1.0, 1.0, iTestGridSize) # sample on a uniform grid
+    aMeshX, aMeshY = np.meshgrid(dx, dx)
+    aTestPoints = np.column_stack([aMeshX.reshape((iTestGridSize**2, 1)),
+                                   aMeshY.reshape((iTestGridSize**2, 1))])
+    aReferenceValues = np.exp(-aTestPoints[:,0]**2) * np.cos(aTestPoints[:,1])
+    aReferenceValues = aReferenceValues.reshape((aReferenceValues.shape[0], 1))
+
+    def testGrid(grid, aTestPoints, aReferenceValues):
+        aResult = grid.evaluateBatch(aTestPoints)
+        for i in range(20):
+            aX = aTestPoints[i,:]
+        return np.max(np.abs(aResult - aReferenceValues))
+
+
+    iInitialLevel = 5
+
+    grid_isotropic = Tasmanian.SparseGrid()
+    grid_iptotal   = Tasmanian.SparseGrid()
+    grid_icurved   = Tasmanian.SparseGrid()
+    grid_surplus   = Tasmanian.SparseGrid()
+    grid_isotropic.makeGlobalGrid(iNumInputs, iNumOutputs, iInitialLevel, "level", "leja")
+    grid_iptotal.copyGrid(grid_isotropic)
+    grid_icurved.copyGrid(grid_isotropic)
+    grid_surplus.copyGrid(grid_isotropic)
+
+    iNumThreads = 1
+    iBudget = 100
+    print("{0:>22s}{1:>22s}{2:>22s}{3:>22s}".format("isotropic", "iptotal", "ipcurved", "surplus"))
+    print("{0:>8s}{1:>14s}{0:>8s}{1:>14s}{0:>8s}{1:>14s}{0:>8s}{1:>14s}".format("points", "error"))
+
+    bBelowBudget = True
+    while(bBelowBudget):
+        sInfo = ""
+        if (grid_isotropic.getNumLoaded() < iBudget):
+            Tasmanian.loadNeededPoints(lambda x, tid : model(x), grid_isotropic, iNumThreads)
+            sInfo += "{0:>8d}{1:>14s}".format(grid_isotropic.getNumLoaded(),
+                     "{0:1.4e}".format(testGrid(grid_isotropic, aTestPoints, aReferenceValues)))
+
+            iLevel = 0
+            while(grid_isotropic.getNumNeeded() == 0):
+                grid_isotropic.updateGlobalGrid(iLevel, "level")
+                iLevel += 1
+        else:
+            sInfo += "{0:>22s}".format("")
+
+        if (grid_iptotal.getNumLoaded() < iBudget):
+            Tasmanian.loadNeededPoints(lambda x, tid : model(x), grid_iptotal, iNumThreads)
+            sInfo += "{0:>8d}{1:>14s}".format(grid_iptotal.getNumLoaded(),
+                     "{0:1.4e}".format(testGrid(grid_iptotal, aTestPoints, aReferenceValues)))
+
+            grid_iptotal.setAnisotropicRefinement("iptotal", 10, 0);
+        else:
+            sInfo += "{0:>22s}".format("")
+
+        if (grid_icurved.getNumLoaded() < iBudget):
+            Tasmanian.loadNeededPoints(lambda x, tid : model(x), grid_icurved, iNumThreads)
+            sInfo += "{0:>8d}{1:>14s}".format(grid_icurved.getNumLoaded(),
+                     "{0:1.4e}".format(testGrid(grid_icurved, aTestPoints, aReferenceValues)))
+
+            grid_icurved.setAnisotropicRefinement("ipcurved", 10, 0);
+        else:
+            sInfo += "{0:>22s}".format("")
+
+        if (grid_surplus.getNumLoaded() < iBudget):
+            Tasmanian.loadNeededPoints(lambda x, tid : model(x), grid_surplus, iNumThreads)
+            sInfo += "{0:>8d}{1:>14s}".format(grid_surplus.getNumLoaded(),
+                     "{0:1.4e}".format(testGrid(grid_surplus, aTestPoints, aReferenceValues)))
+
+            grid_surplus.setSurplusRefinement(1.E-8, 0)
+        else:
+            sInfo += "{0:>22s}".format("")
+
+        print(sInfo)
+        bBelowBudget = (grid_isotropic.getNumLoaded() < iBudget
+                       or grid_icurved.getNumLoaded() < iBudget
+                       or grid_icurved.getNumLoaded() < iBudget
+                       or grid_surplus.getNumLoaded() < iBudget)
+
 
 if (__name__ == "__main__"):
     example_05()
