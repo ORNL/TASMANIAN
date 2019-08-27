a069018a5f37aa0bd76bf55c9b847c0a278c2871
Miroslav Stoyanov <stoyanovmk@ornl.gov>
2019-08-27 12:48:33 +0300

    added file structure

---
+++ SparseGrids/Examples/example_sparse_grids_07.cpp
@@ -0,0 +1,45 @@
+
+#include "Tasmanian.hpp"
+#include <random>
+
+using namespace std;
+
+/*!
+ * \file example_sparse_grids_07.cpp
+ * \brief Examples for the Tasmanian Sparse Grid module.
+ * \author Miroslav Stoyanov
+ * \ingroup TasmanianSGExamples
+ *
+ * Tasmanian Sparse Grids Example 7.
+ */
+
+/*!
+ * \ingroup TasmanianSGExamples
+ * \addtogroup TasmanianSGExamples7 Tasmanian Sparse Grids module, example 7
+ *
+ * \par Example 7
+ * Benchmark Global vs. Sequence grids.
+ */
+
+/*!
+ * \ingroup TasmanianSGExamples7
+ * \brief Sparse Grids Example 7: Global vs. Sequence grids
+ *
+ *
+ * \snippet SparseGrids/Examples/example_sparse_grids_07.cpp SG_Example_07 example
+ */
+void sparse_grids_example_07(){
+#ifndef __TASMANIAN_DOXYGEN_SKIP
+//! [SG_Example_07 example]
+#endif
+
+    cout << "\n---------------------------------------------------------------------------------------------------\n";
+    cout << std::scientific; cout.precision(4);
+    cout << "Example 6: interpolate f(x,y) = exp(-x^2) * exp(y) * cos(z), using the rleja rule\n"
+         << "           employs adaptive construction\n";
+
+
+#ifndef __TASMANIAN_DOXYGEN_SKIP
+//! [SG_Example_07 example]
+#endif
+}
