22fccc3876f1514902a43daa758bfff6ea9601b4
Miroslav Stoyanov <stoyanovmk@ornl.gov>
2019-09-07 15:59:07 +0200

    added example 3

--- InterfacePython/example_dream_03.py
+++ InterfacePython/example_dream_03.py
@@ -47,9 +47,68 @@ def example_03():
     print("             -- problem setup --")
     print("           t in [0,1], t is discretized with 32 equidistant nodes")
     print("           the likelihood is exp(- 16 * (f(x) - d)^2)")
-    print("           using a sparse grid to interpolate the model")
-    print("     NOTE: 16 = 32/2 corresponds to the discretization error in t\n")
+    print("           using a sparse grid to interpolate the model\n")
 
+    iNumDimensions = 2
+    iNumOutputs = 32
+    iNumChains = 500
+    iNumBurnupIterations = 1000
+    iNumCollectIterations = 300
+
+    def model(aX):
+        # using 32 nodes in the interior of (0.0, 1.0)
+        t = numpy.linspace(1.0 / 64.0, 1.0 - 1.0 / 64.0, 32)
+        return numpy.sin(numpy.pi * aX[0] * t + aX[1])
+
+    aData = model([5.0, 0.3 * numpy.pi]) + model([10.0, 0.1 * numpy.pi])
+
+    grid = makeSequenceGrid(iNumDimensions, iNumOutputs, 30, 'iptotal', 'leja')
+
+    domain_lower = numpy.array([ 1.0, -0.1])
+    domain_upper = numpy.array([12.0,  1.7])
+    grid.setDomainTransform(numpy.column_stack([domain_lower, domain_upper]))
+
+    loadNeededPoints(lambda x, tid : model(x), grid, 2)
+
+    likely = DREAM.LikelihoodGaussIsotropic(1.0 / float(iNumOutputs), aData)
+
+    state = DREAM.State(iNumChains, grid)
+    state.setState(DREAM.genUniformSamples(domain_lower, domain_upper, state.getNumChains()))
+
+    DREAM.Sample(iNumBurnupIterations, iNumCollectIterations,
+                 DREAM.Posterior(grid, likely, prior = "uniform"),
+                 DREAM.Domain(grid),
+                 state,
+                 DREAM.IndependentUpdate("gaussian", 0.01),
+                 DREAM.DifferentialUpdate(90),
+                 typeForm = DREAM.typeLogform)
+
+    aSamples = state.getHistory()
+
+    # split the frequencies into low and high for the two modes of the posterior
+    # here 6.5 is the mid-point of the domain
+    low_frequency = numpy.average(numpy.array(
+        [aSamples[i,0] for i in range(aSamples.shape[0]) if aSamples[i,0] < 6.5]
+    ))
+    low_correction = numpy.average(numpy.array(
+        [aSamples[i,1] for i in range(aSamples.shape[0]) if aSamples[i,0] < 6.5]
+    ))
+    high_frequency = numpy.average(numpy.array(
+        [aSamples[i,0] for i in range(aSamples.shape[0]) if aSamples[i,0] >= 6.5]
+    ))
+    high_correction = numpy.average(numpy.array(
+        [aSamples[i,1] for i in range(aSamples.shape[0]) if aSamples[i,0] >= 6.5]
+    ))
+
+    print("Inferred values:")
+    print(" low   frequency:{0:13.6f}   error:{1:14.6e}".format(low_frequency,
+                                                     numpy.abs(low_frequency - 5.0)))
+    print(" low  correction:{0:13.6f}   error:{1:14.6e}\n".format(low_correction,
+                                                     numpy.abs(low_correction - 0.3 * numpy.pi)))
+    print(" high  frequency:{0:13.6f}   error:{1:14.6e}".format(high_frequency,
+                                                     numpy.abs(high_frequency - 10.0)))
+    print(" high correction:{0:13.6f}   error:{1:14.6e}\n".format(high_correction,
+                                                    numpy.abs(high_correction - 0.1 * numpy.pi)))
 
 if __name__ == "__main__":
     example_03()
