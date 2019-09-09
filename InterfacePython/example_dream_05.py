4fd3ce623f49eaec02f6544ee4518750f87482b7
mkstoyanov <stoyanovmk@ornl.gov>
2019-09-09 15:27:20 -0400

    added example 5

--- InterfacePython/example_dream_05.py
+++ InterfacePython/example_dream_05.py
@@ -42,5 +42,84 @@ def example_05():
     print("           using two different likelihood functions, corresponding to l-2 and l-1 norms")
     print("           looking for the mode of the posterior, i.e., the optimal fit to the data\n")
 
+    iNumDimensions = 5
+    iNumChains = 50
+    iNumBurnupIterations  = 1000
+    iNumCollectIterations = 1000
+    fScaleT = 1.0 / 64.0 # scale in T
+
+    def model(aX):
+        # the model assumes 2D array aX
+        t = numpy.linspace(1.0 / 128.0, 1.0 - 1.0 / 128.0, 64)
+        aSinMatrix = numpy.column_stack([numpy.sin(float(k) * numpy.pi * t) for k in range(1, 6)])
+        return numpy.matmul(aSinMatrix, aX.transpose()).transpose()
+
+    aNominalWeights = numpy.array([0.0, 2.0, 0.0, 1.0, 0.0])
+    aData = model(aNominalWeights).reshape((64,))
+    aData = aData + DREAM.genUniformSamples([-fScaleT for i in range(64)],
+                                            [fScaleT for i in range(64)], 1).reshape((64,))
+
+    likely = DREAM.LikelihoodGaussIsotropic(fScaleT, aData)
+
+    domain_lower = numpy.array([0.0 for i in range(iNumDimensions)])
+    domain_upper = numpy.array([3.0 for i in range(iNumDimensions)])
+
+    state = DREAM.State(iNumChains, iNumDimensions)
+    state.setState(DREAM.genUniformSamples(domain_lower, domain_upper, state.getNumChains()))
+
+    DREAM.Sample(iNumBurnupIterations, iNumCollectIterations,
+                 DREAM.Posterior(model, likely, prior = "uniform", typeForm = DREAM.typeLogform),
+                 DREAM.Domain("hypercube", domain_lower, domain_upper),
+                 state,
+                 DREAM.IndependentUpdate("uniform", 0.0),
+                 DREAM.DifferentialUpdate(100),
+                 typeForm = DREAM.typeLogform)
+
+    aSolution = state.getApproximateMode()
+
+    sResult = ""
+    sErrors = ""
+    for i in range(iNumDimensions):
+        sResult = "{0:1s}{1:13.5f}".format(sResult, aSolution[i])
+        sErrors = "{0:1s}{1:13.5e}".format(sErrors, numpy.abs(aSolution[i] - aNominalWeights[i]))
+    print("Using Gaussian likelihood, the computed solution is:")
+    print(" computed: {0:1s}".format(sResult))
+    print("    error: {0:1s}".format(sErrors))
+
+    ###############################################################################################
+    # repeat the example, but change the likelihood
+    ###############################################################################################
+    def l1likelihood(aX, aData):
+        # computes the likelihood, the 32 comes from the discretization in t
+        aY = model(aX)
+        aResult = numpy.empty((aY.shape[0],), numpy.float64)
+        for i in range(aY.shape[0]):
+            aResult[i] = -32.0 * numpy.sum(numpy.abs(aY[i,:] - aData))
+        return aResult
+
+    state = DREAM.State(iNumChains, iNumDimensions) # reset the state
+    state.setState(DREAM.genUniformSamples(domain_lower, domain_upper, state.getNumChains()))
+
+    DREAM.Sample(iNumBurnupIterations, iNumCollectIterations,
+                 DREAM.PosteriorEmbeddedLikelihood(lambda x : l1likelihood(x, aData),
+                                                   prior = "uniform",
+                                                   typeForm = DREAM.typeLogform),
+                 DREAM.Domain("hypercube", domain_lower, domain_upper),
+                 state,
+                 DREAM.IndependentUpdate("uniform", 0.0),
+                 DREAM.DifferentialUpdate(100),
+                 typeForm = DREAM.typeLogform)
+
+    aSolution = state.getApproximateMode()
+
+    sResult = ""
+    sErrors = ""
+    for i in range(iNumDimensions):
+        sResult = "{0:1s}{1:13.5f}".format(sResult, aSolution[i])
+        sErrors = "{0:1s}{1:13.5e}".format(sErrors, numpy.abs(aSolution[i] - aNominalWeights[i]))
+    print("\nUsing l-1 likelihood, the computed solution is:")
+    print(" computed: {0:1s}".format(sResult))
+    print("    error: {0:1s}".format(sErrors))
+
 if __name__ == "__main__":
     example_05()
