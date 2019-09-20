8c1b6625a1d69c540bd30e91bc087be37b9e0dea
mkstoyanov <stoyanovmk@ornl.gov>
2019-09-20 18:04:16 -0400

    documenting the dream python api

--- InterfacePython/TasmanianDreamSampler.py
+++ InterfacePython/TasmanianDreamSampler.py
@@ -113,7 +113,40 @@ def iupdateWrapper(callableIUpdate, iNumDims, pX):
 
 
 class Domain(object):
+    '''
+    Domain objects specify the domain of sampling and is used to test
+    each candidate sample. The objects roughly correspond to the inside()
+    parameter given to TasDREAM::SampleDREAM() but with extra flexibility
+    to reduce the cost of C++ to Python callbacks.
+
+    The domain can be specified in one of the following ways:
+    1) Domain(grid)
+       The grid is an instance of Tasmanian.SparseGrid() and the domain
+       will match the domain of the sparse grid. The grid domain is usually
+       a hypercube with lower and upper limit, but in some cases, e.g.,
+       using gauss-hermite and gauss-laguerre rules, the domain can be
+       unbounded in some directions.
+    2) Domain("hypercube", aLower, aUpper)
+       The domain will be set to a hypercube, similar to TasDREAM::hypercube().
+       The aLower and aUpper must be one dimensional numpy.ndarrays with
+       size equal to the dimensions of the domain and the corresponding
+       values must specify the lower and upper limit in each direction.
+    3) Domain("unbounded")
+       Samples will be accepted and rejected only based on the probability
+       density, i.e., the sampling domain includes all real valued vectors.
+    4) Domain("custom", inside)
+       The inside parameter must be a callable object that accepts an single
+       numpy.ndarray with size equal to the number of dimensions, and must
+       return True or False depending on whether the vector is inside
+       the domain.
+    '''
     def __init__(self, sType, *args):
+        '''
+        Constructor that translates the input arguments into inputs
+        to the internal Tasmanian API.
+
+        See help(Tasmanian.DREAM.Domain)
+        '''
         self.TasmanianDreamDomain = True
         if hasattr(sType, "TasmanianSparseGridObject"):
             self.pGrid = sType.pGrid
@@ -140,7 +173,31 @@ class Domain(object):
 
 
 class IndependentUpdate(object):
+    '''
+    Each step of the DREAM procedure uses two updates to generate new proposed
+    vectors for each chain. The independent update is a zero-mean random vector
+    pulled from a distribution that does not depend on the other DREAM chains.
+    Thus is the Python equivalent to the independent_update() parameter
+    to TasDREAM::SampleDREAM().
+
+    The independent update can be specified in one of the following ways:
+    1) IndependentUpdate(sType, fMagnitude)
+       The sType is a string and fMagnitude is a float64 corresponding to the
+       dist and magnitude of the TasDREAM::SampleDREAM() overload.
+       The sType must be either "gaussian" or "uniform".
+       Note that the magnitude can be zero.
+    2) IndependentUpdate(iupdate)
+       The iupdate must be a callable object that accepts a numpy.ndarray
+       with length equal to the sampling dimension and returns same vector
+       but perturbed with the update, e.g., lambda x : x + update().
+    '''
     def __init__(self, sType, callableOrMagnitude = 0.0):
+        '''
+        Constructor that translates the input arguments into inputs
+        to the internal Tasmanian API.
+
+        See help(Tasmanian.DREAM.IndependentUpdate)
+        '''
         self.TasmanianDreamIndependentUpdate = True
         if not ((sys.version_info.major == 3 and isinstance(sType, str))
            or (sys.version_info.major == 2 and isinstance(sType, basestring))):
@@ -155,7 +212,34 @@ class IndependentUpdate(object):
 
 
 class DifferentialUpdate(object):
+    '''
+    Each step of the DREAM procedure uses two updates to generate new proposed
+    vectors for each chain. The differential update is defined as a scale
+    multiplied by the difference between two of the DREAM chains.
+    This class sets the scale, i.e., the magnitude of the differential update.
+    Thus is the Python equivalent to the differential_update() parameter
+    to TasDREAM::SampleDREAM().
+
+    The differential update can be specified in one of the following ways:
+    1) DifferentialUpdate(iMagnitude)
+       The iMagnitude is an integer that defines the percent scale of the
+       differential update, i.e., the update is float(iMagnitude) / 100.0
+       This is equivalent to using TasDREAM::const_percent<iMagnitude>
+       in the C++ call to TasDREAM::SampleDREAM().
+    2) DifferentialUpdate(dupdate)
+       The dupdate must be a callable object that accepts no inputs
+       and returns a float64 indicating the magnitude.
+
+    Note: by symmetry, positive and negative scales with the same magnitude
+          are statistically equivalent.
+    '''
     def __init__(self, callableOrMagnitude):
+        '''
+        Constructor that translates the input arguments into inputs
+        to the internal Tasmanian API.
+
+        See help(Tasmanian.DREAM.DifferentialUpdate)
+        '''
         self.TasmanianDreamDifferentialUpdate = True
         if (((sys.version_info.major == 3) and isinstance(callableOrMagnitude, int))
             or ((sys.version_info.major == 2) and isinstance(callableOrMagnitude, (int, long)))):
@@ -167,7 +251,37 @@ class DifferentialUpdate(object):
 
 
 class RandomGenerator(object):
+    '''
+    A class that helps specify the random number generator to be used on the
+    C++ side of the call to TasDREAM::SampleDREAM().
+
+    The random number engine can be defined in one of several ways:
+    1) RandomGenerator("default") or just RandomGenerator()
+       The C++ code will use the rand() command with seed initialized from
+       the system time.
+    2) RandomGenerator("minstd_rand")
+       The rand() command in C++ is implementation dependent, i.e., the random
+       number may be different between compilers even if the same seed is used.
+       The std::minstd_rand generator is defined in the standard and the output
+       is implementation independent, which makes it a good option for
+       testing and debugging (probably not very good for actual applications).
+       The random seed used here will still be taken from the system time.
+    3) RandomGenerator("minstd_rand", iSeed)
+       In addition to the standard std::minstd_rand engine this also specifies
+       the seed.
+    4) RandomGenerator(callableRNG = random01)
+       The random01 parameter must be a callable object that returns random
+       numbers distributed uniformly in (0, 1), e.g.,
+       from random import uniform
+       RandomGenerator(callableRNG = lambda : uniform(0.0, 1.0))
+    '''
     def __init__(self, sType = "default", iSeed = -1, callableRNG = lambda : 1):
+        '''
+        Constructor that translates the input arguments into inputs
+        to the internal Tasmanian API.
+
+        See help(Tasmanian.DREAM.RandomGenerator)
+        '''
         self.TasmanianDreamRandomGenerator = True
         self.sType = sType
         if (sys.version_info.major == 3):
@@ -175,11 +289,63 @@ class RandomGenerator(object):
         self.iSeed = iSeed
         self.pCallable = type_dream_random(callableRNG)
 
-typePriorCallable = 0
+typePriorCallable = 0 # internal API, the values are synchronized with the C/C++ code
 typePriorUniform  = 1
 
 class Posterior(object):
+    '''
+    A class that helps combine the components of a Bayesian posterior into
+    a single callable object. Strictly speaking, this is a factory class that
+    generates probability distribution callable objects used in DREAM.Sample().
+    The intended use of the class is embedded as in the C++ function
+    TasDREAM::posterior().
+
+    The probability distribution takes a two dimensional numpy.ndarray
+    object with shape (num_samples, num_dimensions) and returns a one
+    dimensional numpy.ndarray with length (num_samples,).
+    Each entry of the result corresponds to the (unscaled) probability
+    density at the corresponding vector.
+
+    The usage of this class is:
+    DREAM.Sample(..., Posterior(model, likelihood, prior), ...)
+    or if log-form sampling is to be used:
+    DREAM.Sample(..., Posterior(model, likelihood, prior, typeForm), ...)
+
+    Combining the three components gives us:
+        result = likelihood(typeForm, model(x)) * prior(x)
+    of if using typeLogform:
+        result = likelihood(typeForm, model(x)) + prior(x)
+
+    The model, likelihood, and prior, can be callable objects so that
+
+    model: input.shape  = (num_samples, num_dimensions)
+           output.shape = (num_samples, num_outputs)
+
+    likelihood: input.shape  = (num_samples, num_outputs)
+                output.shape = (num_samples,)
+
+    prior: input.shape  = (num_samples, num_dimensions)
+           output.shape = (num_samples,)
+
+    In addition:
+    model: can be an instance of Tasmanian.SparseGrid()
+    likelihood: can be an instance of DREAM.LikelihoodGaussIsotropic()
+                of LikelihoodGaussAnisotropic().
+    prior: can be the string "uniform"
+
+    The typeForm must be either typeRegform (default) or typeLogform
+    and must be synchronized with the form used in DREAM.Sample().
+    The likelihood need only implement the form used in DREAM.Sample().
+    The form here indicates whether to add or multiply by the log of
+    the values of the prior.
+    '''
     def __init__(self, model, likelihood, prior, typeForm = typeRegform):
+        '''
+        Constructor that translates the input arguments into inputs
+        to the internal Tasmanian API.
+
+        See help(Tasmanian.DREAM.Posterior)
+        '''
         self.TasmanianPosterior = True
 
         self.typeForm = typeForm
@@ -211,18 +377,27 @@ class Posterior(object):
             raise InputError("prior", "prior should be either 'uniform' or a callable object.")
 
     def doModel(self, aX):
+        '''
+        Internal API, applies the model part of the posterior.
+        '''
         if self.bUseSparsegrid:
             return self.model.evaluateBatch(aX)
         else:
             return self.model(aX)
 
     def doLikelihood(self, aModelValues):
+        '''
+        Internal API, applies the likelihood part of the posterior.
+        '''
         if self.bUseTasLikely:
             return self.likelihood.getLikelihood(self.typeForm, aModelValues)
         else:
             return self.likelihood(self.typeForm, aModelValues)
 
     def doPrior(self, aLikely, aX):
+        '''
+        Internal API, computes the prior part of the posterior.
+        '''
         if self.typePrior == typePriorUniform:
             aResult = aLikely
         else:
@@ -234,9 +409,17 @@ class Posterior(object):
         return aResult
 
     def distribution(self):
+        '''
+        Internal API, combines the input objects into a single lambda.
+        '''
         return lambda x : self.doPrior(self.doLikelihood(self.doModel(x)), x).reshape((x.shape[0],))
 
 def PosteriorEmbeddedLikelihood(model, prior, typeForm = typeRegform):
+    '''
+    Shortcut to Tasmanian.DREAM.Posterior() when the model and the likelihood
+    are combined into a single object (callable or sparse grid).
+    The prior and typeForm are the same.
+    '''
     return Posterior(model, lambda t, x : x, prior, typeForm)
 
 def genUniformSamples(lfLower, lfUpper, iNumSamples, random01 = RandomGenerator(sType = "default")):
@@ -301,6 +484,56 @@ def Sample(iNumBurnup, iNumCollect,
            typeForm = typeRegform):
     '''
     Wrapper to TasDREAM::SampleDREAM().
+    The Markov Chain Monte Carlo algorithms generate chains of samples
+    with distribution that converges to the target probability density.
+    The current state of the chains (i.e., the last vector) is updated by
+    first generating a set of proposals and then accepting/rejecting randomly
+    but with the constraint that the only accepted vectors belong to the
+    specified domain. The proposals are constructed from two components,
+    the independent component that is iid random with zero mean and
+    the differential component that is takes from the scaled difference
+    between the state of two randomly selected chains.
+
+    iNumBurnup: is a non-negative integer that indicates the number of
+                iterations to discard, i.e., samples that have no statistical
+                significance because the chain state has not converged to
+                the target probability distribution.
+    iNumCollect: is a non-negative integer that indicates the number of
+                iterations to store in the state history.
+
+    probability_distibution: is either an instance of DREAM.Posterior() or
+                a callable object that accepts a two dimensional numpy.ndarray
+                and then returns a one dimensional numpy.ndarray.
+                See help(Tasmanian.DREAM.Posterior)
+                Can also use a lambda to capture a Tasmanian sparse grid, i.e.,
+                lambda x : grid.evaluateBatch(x).reshape((x.shape[0],))
+
+    domain_description: is an instance of DREAM.Domain()
+                See help(Tasmanian.DREAM.Domain)
+
+    dream_state: is an instance of DREAM.State() with number of dimensions
+                that match the dimensions used by the probability_distibution.
+                See help(Tasmanian.DREAM.State)
+                On input the state chains have to be initialized, on output
+                the history will be populated with the iNumCollect samples.
+                (if the history is not empty, the new samples will be appended)
+
+    independent_update: is an instance of DREAM.IndependentUpdate()
+                See help(Tasmanian.DREAM.IndependentUpdate)
+
+    differential_update: is an instance of DREAM.DifferentialUpdate()
+                See help(Tasmanian.DREAM.DifferentialUpdate)
+
+    random01: is an instance of DREAM.RandomGenerator()
+                See help(Tasmanian.DREAM.RandomGenerator)
+
+    typeForm: is either DREAM.typeRegform or DREAM.typeLogform
+               The form modifies the accept/reject criteria based on whether
+               the probability_distibution returns the actual distribution
+               of interest or the logarithm of that (which is sometimes more
+               stable and/or easier to approximate with a sparse grid).
+
+    Note: the typeForm must match the return of the probability_distibution.
     '''
     if not hasattr(domain_description, "TasmanianDreamDomain"):
         raise InputError("domain_description", "domain_description must be an instance of DREAM.Domain()")
