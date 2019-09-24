baedbe9c351c85e4f82a5a742015bf9b53ce8ac5
mkstoyanov <stoyanovmk@ornl.gov>
2019-09-24 14:07:53 -0400

    added example 4

---
+++ InterfacePython/example_sparse_grids_04.py
@@ -0,0 +1,64 @@
+##############################################################################################################################################################################
+# Copyright (c) 2017, Miroslav Stoyanov
+#
+# This file is part of
+# Toolkit for Adaptive Stochastic Modeling And Non-Intrusive ApproximatioN: TASMANIAN
+#
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+#    and the following disclaimer in the documentation and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse
+#    or promote products derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
+# OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA,
+# OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# UT-BATTELLE, LLC AND THE UNITED STATES GOVERNMENT MAKE NO REPRESENTATIONS AND DISCLAIM ALL WARRANTIES, BOTH EXPRESSED AND IMPLIED.
+# THERE ARE NO EXPRESS OR IMPLIED WARRANTIES OF MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE, OR THAT THE USE OF THE SOFTWARE WILL NOT INFRINGE ANY PATENT,
+# COPYRIGHT, TRADEMARK, OR OTHER PROPRIETARY RIGHTS, OR THAT THE SOFTWARE WILL ACCOMPLISH THE INTENDED RESULTS OR THAT THE SOFTWARE OR ITS USE WILL NOT RESULT IN INJURY OR DAMAGE.
+# THE USER ASSUMES RESPONSIBILITY FOR ALL LIABILITIES, PENALTIES, FINES, CLAIMS, CAUSES OF ACTION, AND COSTS AND EXPENSES, CAUSED BY, RESULTING FROM OR ARISING OUT OF,
+# IN WHOLE OR IN PART THE USE, STORAGE OR DISPOSAL OF THE SOFTWARE.
+##############################################################################################################################################################################
+
+import numpy as np
+import Tasmanian
+
+def example_04():
+
+    print("\n---------------------------------------------------------------------------------------------------\n")
+    print("Example 4: interpolate f(x,y) = exp(-x^2) * cos(y), using clenshaw-curtis iptotal rule")
+
+    iNumInputs = 2
+    iNumOutputs = 1
+
+    aPointOfInterest = np.array([0.3, 0.7])
+    aReferenceSolution = np.exp(-aPointOfInterest[0]**2) * np.cos(aPointOfInterest[1])
+
+    for prec in [6, 12]:
+        grid = Tasmanian.makeGlobalGrid(iNumInputs, iNumOutputs, prec,
+                                        "iptotal", "clenshaw-curtis")
+        aPoints = grid.getNeededPoints()
+
+        aModelValues = np.exp(-aPoints[:,0]**2) * np.cos(aPoints[:,1])
+        grid.loadNeededPoints(aModelValues.reshape((aModelValues.shape[0], iNumOutputs)))
+
+        # when using multiple points at once, evaluateBatch() is more efficient
+        aResult = grid.evaluate(aPointOfInterest)
+        fError = np.abs(aResult[0] - aReferenceSolution)
+
+        print("\n    using total degree polynomials: {0:>2d}th degree".format(prec))
+        print("                      the grid has: {0:1d} points".format(grid.getNumPoints()))
+        print("          interpolant at (0.3,0.7): {0:1.5e}".format(aResult[0]))
+        print("                             error: {0:1.5e}".format(fError))
+
+
+if (__name__ == "__main__"):
+    example_04()
