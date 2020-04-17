63ef82e15e6d453e57bc0c5fe34ec14304df3293
Miroslav Stoyanov <stoyanovmk@ornl.gov>
2020-04-17 16:49:43 -0400

    added multi-rhs least-squares solver

--- InterfaceTPL/tsgBlasWrappers.hpp
+++ InterfaceTPL/tsgBlasWrappers.hpp
@@ -73,11 +73,25 @@ void dgemm_(const char* transa, const char* transb, const int *m, const int *n, 
             const double *A, const int *lda, const double *B, const int *ldb, const double *beta, const double *C, const int *ldc);
 void dtrsm_(const char *side, const char *uplo, const char *trans, const char *diag, const int *M, const int *N,
             const double *alpha, const double *A, const int *lda, double *B, const int *ldb);
+void ztrsm_(const char *side, const char *uplo, const char *trans, const char *diag, const int *M, const int *N,
+            const std::complex<double> *alpha, const std::complex<double> *A, const int *lda, std::complex<double> *B, const int *ldb);
 // LAPACK solvers
+// General PLU factorize/solve
 void dgetrf_(const int *M, const int *N, double *A, const int *lda, int *ipiv, int *info);
 void dgetrs_(const char *trans, const int *N, const int *nrhs, const double *A, const int *lda, const int *ipiv, double *B, const int *ldb, int *info);
+// General least-squares solve
 void dgels_(const char *trans, const int *M, const int *N, const int *nrhs, double *A, const int *lda,
             double *B, const int *ldb, double *work, int *lwork, int *info);
+void zgels_(const char *trans, const int *M, const int *N, const int *nrhs, std::complex<double> *A, const int *lda,
+            std::complex<double> *B, const int *ldb, std::complex<double> *work, int *lwork, int *info);
+// General LQ-factorize and multiply by Q
+void dgelq_(const int *M, const int *N, double *A, const int *lda, double *T, int const *Tsize, double *work, int const *lwork, int *info);
+void dgemlq_(const char *side, const char *trans, const int *M, const int *N, const int *K, double const *A, int const *lda,
+             double const *T, int const *Tsize, double C[], int const *ldc, double *work, int const *lwork, int *info);
+void zgelq_(const int *M, const int *N, std::complex<double> *A, const int *lda, std::complex<double> *T, int const *Tsize,
+            std::complex<double> *work, int const *lwork, int *info);
+void zgemlq_(const char *side, const char *trans, const int *M, const int *N, const int *K, std::complex<double> const *A, int const *lda,
+             std::complex<double> const *T, int const *Tsize, std::complex<double> C[], int const *ldc, std::complex<double> *work, int const *lwork, int *info);
 }
 #endif
 
@@ -112,9 +126,14 @@ namespace TasBLAS{
                      double beta, double C[], int ldc){
         dgemm_(&transa, &transb, &M, &N, &K, &alpha, A, &lda, B, &ldb, &beta, C, &ldc);
     }
-    //! \brief BLAS trsm
+    //! \brief BLAS dtrsm
     inline void trsm(char side, char uplo, char trans, char diag, int M, int N, double alpha, double const A[], int lda, double B[], int ldb){
         dtrsm_(&side, &uplo, &trans, &diag, &M, &N, &alpha, A, &lda, B, &ldb);
+    }
+    //! \brief BLAS ztrsm
+    inline void trsm(char side, char uplo, char trans, char diag, int M, int N, std::complex<double> alpha,
+                     std::complex<double> const A[], int lda, std::complex<double> B[], int ldb){
+        ztrsm_(&side, &uplo, &trans, &diag, &M, &N, &alpha, A, &lda, B, &ldb);
     }
     //! \brief LAPACK dgetrf
     inline void getrf(int M, int N, double A[], int lda, int ipiv[]){
@@ -137,6 +156,63 @@ namespace TasBLAS{
                 throw std::runtime_error(std::string("Lapack dgels_ solve-stage exited with code: ") + std::to_string(info));
             else
                 throw std::runtime_error(std::string("Lapack dgels_ infer-worksize-stage exited with code: ") + std::to_string(info));
+        }
+    }
+    //! \brief LAPACK zgels
+    inline void gels(char trans, int M, int N, int nrhs, std::complex<double> A[], int lda, std::complex<double> B[], int ldb, std::complex<double> work[], int lwork){
+        int info = 0;
+        zgels_(&trans, &M, &N, &nrhs, A, &lda, B, &ldb, work, &lwork, &info);
+        if (info != 0){
+            if (lwork > 0)
+                throw std::runtime_error(std::string("Lapack zgels_ solve-stage exited with code: ") + std::to_string(info));
+            else
+                throw std::runtime_error(std::string("Lapack zgels_ infer-worksize-stage exited with code: ") + std::to_string(info));
+        }
+    }
+    //! \brief LAPACK dgeql
+    inline void geql(int M, int N, double A[], int lda, double T[], int Tsize, double work[], int lwork){
+        int info = 0;
+        dgelq_(&M, &N, A, &lda, T, &Tsize, work, &lwork, &info);
+        if (info != 0){
+            if (lwork > 0)
+                throw std::runtime_error(std::string("Lapack dgeql_ factorize-stage exited with code: ") + std::to_string(info));
+            else
+                throw std::runtime_error(std::string("Lapack dgeql_ infer-worksize-stage exited with code: ") + std::to_string(info));
+        }
+    }
+    //! \brief LAPACK dgemlq
+    inline void gemlq(char side, char trans, int M, int N, int K, double const A[], int lda, double const T[], int Tsize,
+                       double C[], int ldc, double work[], int lwork){
+        int info = 0;
+        dgemlq_(&side, &trans, &M, &N, &K, A, &lda, T, &Tsize, C, &ldc, work, &lwork, &info);
+        if (info != 0){
+            if (lwork > 0)
+                throw std::runtime_error(std::string("Lapack dgemlq_ compute-stage exited with code: ") + std::to_string(info));
+            else
+                throw std::runtime_error(std::string("Lapack dgemlq_ infer-worksize-stage exited with code: ") + std::to_string(info));
+        }
+    }
+        //! \brief LAPACK zgeql
+    inline void geql(int M, int N, std::complex<double> A[], int lda, std::complex<double> T[], int Tsize, std::complex<double> work[], int lwork){
+        int info = 0;
+        zgelq_(&M, &N, A, &lda, T, &Tsize, work, &lwork, &info);
+        if (info != 0){
+            if (lwork > 0)
+                throw std::runtime_error(std::string("Lapack zgeql_ factorize-stage exited with code: ") + std::to_string(info));
+            else
+                throw std::runtime_error(std::string("Lapack zgeql_ infer-worksize-stage exited with code: ") + std::to_string(info));
+        }
+    }
+    //! \brief LAPACK zgemlq
+    inline void gemlq(char side, char trans, int M, int N, int K, std::complex<double> const A[], int lda, std::complex<double> const T[], int Tsize,
+                       std::complex<double> C[], int ldc, std::complex<double> work[], int lwork){
+        int info = 0;
+        zgemlq_(&side, &trans, &M, &N, &K, A, &lda, T, &Tsize, C, &ldc, work, &lwork, &info);
+        if (info != 0){
+            if (lwork > 0)
+                throw std::runtime_error(std::string("Lapack zgemlq_ compute-stage exited with code: ") + std::to_string(info));
+            else
+                throw std::runtime_error(std::string("Lapack zgemlq_ infer-worksize-stage exited with code: ") + std::to_string(info));
         }
     }
 
@@ -164,6 +240,72 @@ namespace TasBLAS{
             }
         }else{ // matrix vector B^T * v = C
             gemv('T', K, N, alpha, B, K, A, 1, beta, C, 1);
+        }
+    }
+    //! \brief Conjugates a matrix, no op in the real case.
+    constexpr inline void conj_matrix(int, int, double[]){}
+    //! \brief Conjugates the matrix, used in the case when 'T' operation is needed by only 'C' is available in the LAPACK standard.
+    inline void conj_matrix(int N, int M, std::complex<double> A[]){
+        for(size_t i=0; i<static_cast<size_t>(N) * static_cast<size_t>(M); i++) A[i] = std::conj(A[i]);
+    }
+    //! \brief Returns the transpose symbol, 'T' in the real case.
+    constexpr inline char get_trans(double){ return 'T'; }
+    //! \brief Returns the conjugate-transpose symbol, 'C' in the complex case.
+    constexpr inline char get_trans(std::complex<double>){ return 'C'; }
+    /*!
+     * \brief Solves the over-determined least squares problem with single right-hand-side.
+     *
+     * Note that trans must be a capital letter N or T.
+     */
+    template<typename scalar_type>
+    inline void solveLS(char trans, int N, int M, scalar_type A[], scalar_type b[]){
+        std::vector<scalar_type> work(1);
+        int n = (trans == 'N') ? N : M;
+        int m = (trans == 'N') ? M : N;
+        char effective_trans = (trans == 'N') ? trans : get_trans(static_cast<scalar_type>(0.0));
+        conj_matrix(N, M, A); // does nothing in the real case, computes the conjugate in the complex one
+        TasBLAS::gels(effective_trans, n, m, 1, A, n, b, N, work.data(), -1);
+        work.resize(static_cast<size_t>(std::real(work[0])));
+        TasBLAS::gels(effective_trans, n, m, 1, A, n, b, N, work.data(), static_cast<int>(work.size()));
+    }
+    /*!
+     * \brief Compute the LQ factorization of the matrix \b A.
+     *
+     * The assumption here is that the matrix is in column major format, otherwise this computes the QR factorization.
+     * In fact, the Tasmanian uses this method to compute QR of a row-major matrix.
+     */
+    template<typename scalar_type>
+    inline void factorizeLQ(int rows, int cols, scalar_type A[], std::vector<scalar_type> &T){
+        T.resize(5);
+        std::vector<scalar_type> work(1);
+        geql(rows, cols, A, rows, T.data(), -1, work.data(), -1);
+        T.resize(static_cast<size_t>(std::real(T[0])));
+        work.resize(static_cast<size_t>(std::real(work[0])));
+        geql(rows, cols, A, rows, T.data(), static_cast<int>(T.size()), work.data(), static_cast<int>(work.size()));
+    }
+    /*!
+     * \brief Multiplies C by the Q factor computed with factorizeLQ.
+     *
+     * Computes \f$ C = C Q^T \f$ where Q comes from the call to factorizeLQ.
+     * The matrix C has dimensions M by N, A has dimensions K by N.
+     */
+    template<typename scalar_type>
+    inline void multiplyQ(int M, int N, int K, scalar_type const A[], std::vector<scalar_type> const &T, scalar_type C[]){
+        std::vector<scalar_type> work(1);
+        gemlq('R', get_trans(static_cast<scalar_type>(0.0)), M, N, K, A, K, T.data(), static_cast<int>(T.size()), C, M, work.data(), -1);
+        work.resize(static_cast<int>(std::real(work[0])));
+        gemlq('R', get_trans(static_cast<scalar_type>(0.0)), M, N, K, A, K, T.data(), static_cast<int>(T.size()), C, M, work.data(), static_cast<int>(work.size()));
+    }
+    //! \brief Solves the least-squares assuming row-major format, see TasmanianDenseSolver::solvesLeastSquares()
+    template<typename scalar_type>
+    void solveLSmulti(int n, int m, scalar_type A[], int nrhs, scalar_type B[]){
+        if (nrhs == 1){
+            TasBLAS::solveLS('T', n, m, A, B);
+        }else{
+            std::vector<scalar_type> T;
+            TasBLAS::factorizeLQ(m, n, A, T);
+            TasBLAS::multiplyQ(nrhs, n, m, A, T, B);
+            TasBLAS::trsm('R', 'L', 'N', 'N', nrhs, m, 1.0, A, m, B, nrhs);
         }
     }
 }
