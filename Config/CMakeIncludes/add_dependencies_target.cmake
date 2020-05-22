6de7399e4af506b50474cc1296d1f52289d4b135
Miroslav Stoyanov <stoyanovmk@ornl.gov>
2020-05-22 11:33:13 -0400

    moved the rpath logic to the dependencies

--- Config/CMakeIncludes/add_dependencies_target.cmake
+++ Config/CMakeIncludes/add_dependencies_target.cmake
@@ -50,3 +50,29 @@ foreach(_tsg_include ${Tasmanian_EXTRA_INCLUDE_DIRS})
 endforeach()
 
 install(TARGETS Tasmanian_dependencies EXPORT "${Tasmanian_export_name}")
+
+########################################################################
+# Create an RPATH list for all dependencies
+########################################################################
+list(APPEND Tasmanian_rpath "${Tasmanian_final_install_path}/lib")
+
+if (Tasmanian_ENABLE_BLAS)
+    foreach(_tsg_lib ${BLAS_LIBRARIES} ${LAPACK_LIBRARIES})
+        get_filename_component(_tsg_libpath ${_tsg_lib} DIRECTORY)
+        list(APPEND Tasmanian_rpath ${_tsg_libpath})
+    endforeach()
+    unset(_tsg_lib)
+    unset(_tsg_libpath)
+endif()
+
+if (Tasmanian_ENABLE_CUDA)
+    list(APPEND Tasmanian_rpath ${Tasmanian_cuda_rpath})
+    unset(Tasmanian_cuda_rpath)
+endif()
+
+if (Tasmanian_ENABLE_MAGMA)
+    list(APPEND Tasmanian_rpath "${Tasmanian_libmagma_dir}")
+    unset(Tasmanian_libmagma_dir)
+endif()
+
+# message(STATUS "Tasmanian RPATH: ${Tasmanian_rpath}")
