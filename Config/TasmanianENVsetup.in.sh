259c21402bd0baea4f942959a67a807a1da821e7
Miroslav Stoyanov <stoyanovmk@ornl.gov>
2021-03-05 18:19:23 -0500

    added workaround that finds and keeps hip version of openmp

--- Config/TasmanianENVsetup.in.sh
+++ Config/TasmanianENVsetup.in.sh
@@ -3,6 +3,9 @@
 # also sets the python path
 export PATH=$PATH:"@Tasmanian_final_install_path@"/bin/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"@Tasmanian_final_install_path@"/lib/
+if [[ "@Tasmanian_ENABLE_HIP@" == "ON" ]]; then
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:"@Tasmanian_hipomp_rpath@"
+fi
 if [[ "@Tasmanian_ENABLE_PYTHON@" == "ON" ]]; then
     export PYTHONPATH=$PYTHONPATH:"@Tasmanian_PYTHONPATH@"
 fi
