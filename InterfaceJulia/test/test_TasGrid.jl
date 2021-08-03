0725214e2e47cb09775640df1396e6a2f8fa42e5
William Kong <wwkong92@gmail.com>
2021-08-03 12:30:44 -0400

    Added more unit tests for TasGrid.jl.

--- InterfaceJulia/test/test_TasGrid.jl
+++ InterfaceJulia/test/test_TasGrid.jl
@@ -20,24 +20,52 @@ end
 # Test the accuracy of a grid with an isotropic total degree lower set and
 # a Clenshaw-Curtis 1D rule.
 @testset "TasGrid" begin
-    @testcase "Clenshaw-Curtis 2D Quadrature" begin
-
-        # Create a grid of total degree 7 and dimension 2.
-        dimension = 2
-        degree = 7
-        grid = create_cc_grid(dimension, degree)
+    @testcase "Clenshaw-Curtis 1D Quadrature" begin
+        # Create a grid of total degree 4 and dimension 1.
+        grid = create_cc_grid(1, 4)
         cc_points = get_points(grid)
         cc_quad_weights = get_quadrature_weights(grid)
-
+        # Compute ∫ₐ sin(x²) dx dy where a=[-1,1].
+        integrand = z -> sin(z[1]^2)
+        integral = sum(cc_quad_weights .* integrand.(cc_points))
+        @test integral ≈ 0.6205366034467622
+        # Compute ∫ₐ exp(-x²) dx dy where a=[-1,1].
+        integrand = z -> exp(-z[1]^2)
+        integral = sum(cc_quad_weights .* integrand.(cc_points))
+        @test integral ≈ 1.493648265624854
+        # Compute ∫ₐ sqrt(arccos(x^2/)) dx dy where a=[-1,1].
+        integrand = z -> sqrt(acos(z[1]^2/2))
+        integral = sum(cc_quad_weights .* integrand.(cc_points))
+        @test integral ≈ 2.3634629916571772
+    end
+    @testcase "Clenshaw-Curtis 2D Quadrature" begin
+        # Create a grid of total degree 6 and dimension 2.
+        grid = create_cc_grid(2, 6)
+        cc_points = get_points(grid)
+        cc_quad_weights = get_quadrature_weights(grid)
         # Compute ∫ₐ∫ₐ sin(x²+y²) dx dy where a=[-1,1].
         integrand = z -> sin(z[1]^2 + z[2]^2)
         integral = sum(cc_quad_weights .* integrand.(cc_points))
         @test integral ≈ 2.245161593287624
-
         # Compute ∫ₐ∫ₐ exp(-(x²+y²)) dx dy where a=[-1,1].
         integrand = z -> exp(-z[1]^2 - z[2]^2)
         integral = sum(cc_quad_weights .* integrand.(cc_points))
         @test integral ≈ 2.230985141404135
+        # Compute ∫ₐ∫ₐ exp(-x²) * cos(y) dx dy where a=[-1,1] (taken from
+        # example_sparse_grids_01.py).
+        integrand = z -> exp(-z[1]^2) * cos(z[2])
+        integral = sum(cc_quad_weights .* integrand.(cc_points))
+        @test integral ≈ 2.513723354063905
+    end
+    @testcase "Clenshaw-Curtis 3D Quadrature" begin
+        # Create a grid of total degree 8 and dimension 3.
+        grid = create_cc_grid(3, 8)
+        cc_points = get_points(grid)
+        cc_quad_weights = get_quadrature_weights(grid)
+        # Compute ∫ₐ∫ₐ∫ₐ exp(-(x²+y²+z²)) dx dy dz where a=[-1,1].
+        integrand = z -> exp(-z[1]^2-z[2]^2-z[3]^2)
+        integral = sum(cc_quad_weights .* integrand.(cc_points))
+        @test integral ≈ 3.332307087093105
     end
 end
 
