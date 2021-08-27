3121cf0790257f8b40531c2ee2b1ae91315a78e0
William Kong <wwkong92@gmail.com>
2021-08-27 09:41:14 -0400

    Small optimizations.

--- InterfacePython/TasmanianAddons.py
+++ InterfacePython/TasmanianAddons.py
@@ -419,5 +419,5 @@ def loadExoticQuadrature(depth, dimension, shift, weight_fn, nref, description, 
     is_symmetric: a boolean that should be set to True it weight_fn is symmetric.
     grid:         a TasmanianSparseGrid to load the data into
     '''
-    pLibCTSG.tsgLoadExoticQuadratureGrid(c_int(depth), c_int(dimension), c_double(shift), type_1Dfunc(weight_fn),
-                                         c_int(nref), description, is_symmetric, grid.pGrid)
+    pLibCTSG.tsgLoadExoticQuadratureGrid(grid.pGrid, c_int(depth), c_int(dimension), c_double(shift), type_1Dfunc(weight_fn),
+                                         c_int(nref), description, is_symmetric)
