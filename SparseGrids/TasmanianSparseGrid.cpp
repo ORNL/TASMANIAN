a99f6a8a56811195f37c94d4caca5322820aa62f
Miroslav Stoyanov <stoyanovmk@ornl.gov>
2021-09-07 11:41:34 -0400

    swapped names in the main c++ interface

--- SparseGrids/TasmanianSparseGrid.cpp
+++ SparseGrids/TasmanianSparseGrid.cpp
@@ -379,11 +379,11 @@ void TasmanianSparseGrid::getInterpolationWeights(const double x[], double weigh
     base->getInterpolationWeights(formCanonicalPoints(x, x_tmp, 1), weights);
 }
 
-void TasmanianSparseGrid::loadNeededPoints(const double *vals){
+void TasmanianSparseGrid::loadNeededValues(const double *vals){
     if (empty()) throw std::runtime_error("Cannot load model values into an empty grid!");
     base->loadNeededPoints(vals);
 }
-void TasmanianSparseGrid::loadNeededPoints(const std::vector<double> &vals){
+void TasmanianSparseGrid::loadNeededValues(const std::vector<double> &vals){
     size_t nump = (size_t) base->getNumNeeded();
     if (nump == 0) nump = (size_t) base->getNumPoints();
     nump *= (size_t) base->getNumOutputs();
