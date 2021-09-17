26b9c9e38852e45a04833242916b0f31a6931fad
William Kong <wwkong92@gmail.com>
2021-09-17 11:40:38 -0400

    Changes to ensure Python 2 compatibility.

--- InterfacePython/TasmanianSG.py
+++ InterfacePython/TasmanianSG.py
@@ -2491,7 +2491,7 @@ class CustomTabulated:
         return pLibTSG.tsgGetQExactCustomTabulated(self.pCustomTabulated, c_int(level))
 
     def getDescription(self):
-        return str(pLibTSG.tsgGetDescriptionCustomTabulated(self.pCustomTabulated), 'UTF-8')
+        return pLibTSG.tsgGetDescriptionCustomTabulated(self.pCustomTabulated).decode('UTF-8')
 
     def getWeightsNodes(self, level):
         '''
