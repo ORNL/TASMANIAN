8a81c7c7056315a1e98c2115a11967ae6eff4e8b
Miroslav Stoyanov <stoyanovmk@ornl.gov>
2021-10-20 09:30:37 -0400

    pip version bump

--- InterfacePython/PipInstaller/setup.py
+++ InterfacePython/PipInstaller/setup.py
@@ -70,7 +70,7 @@ if isosxframework:
 # call the actual package setup command
 setup(
     name='Tasmanian',
-    version='7.7',
+    version='7.8rc1',
     author='Miroslav Stoyanov',
     author_email='stoyanovmk@ornl.gov',
     description='UQ library for sparse grids and Bayesian inference',
