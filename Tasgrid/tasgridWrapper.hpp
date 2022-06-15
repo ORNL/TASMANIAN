da92efc8f79c543e54235058a3ff00adc3d5d6b6
William Kong <wwkong92@gmail.com>
2022-06-15 16:18:23 -0400

    Small optimizations.

--- Tasgrid/tasgridWrapper.hpp
+++ Tasgrid/tasgridWrapper.hpp
@@ -188,9 +188,9 @@ protected:
 
     // Overloads.
     template<typename T>
-    inline void writeMatrix(std::string const &filename, Data2D<T> mat) { writeMatrix(filename, mat.getNumStrips(), mat.getStride(), mat.data()); }
+    inline void writeMatrix(std::string const &filename, Data2D<T> &mat) { writeMatrix(filename, mat.getNumStrips(), mat.getStride(), mat.data()); }
     template<typename T>
-    inline void printMatrix(Data2D<T> mat, bool isComplex = false) { printMatrix(mat.getNumStrips(), mat.getStride(), mat.data(), isComplex); }
+    inline void printMatrix(const Data2D<T> &mat, bool isComplex = false) { printMatrix(mat.getNumStrips(), mat.getStride(), mat.data(), isComplex); }
 
   private:
     TasmanianSparseGrid grid;
