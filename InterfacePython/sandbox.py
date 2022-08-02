f3d2b3cee08ea2b2dfbe6ea71f0ed7ed948e68c9
William Kong <wwkong92@gmail.com>
2022-08-02 16:04:33 -0400

    Clean up sandbox.

--- InterfacePython/sandbox.py
+++ InterfacePython/sandbox.py
@@ -21,34 +21,3 @@ import math
 ###############################################################################
 
 print("Add code to this file to test, debug, or develop features")
-
-x0 = np.array([1.0, 2.0, 3.0], np.float64)
-stepsize = 1/4
-adaptive_stepsize = 0.001 * stepsize
-func = lambda x : x[0] * x[0] / 2 + x[1] * x[1] + 2.0 * x[2] * x[2]
-grad = lambda x : np.array([x[0], 2 * x[1], 4.0 * x[2]], np.float64)
-
-# Constant GD
-gds = Tasmanian.Optimization.GradientDescentState(x0, adaptive_stepsize)
-status = Tasmanian.Optimization.GradientDescent(grad, stepsize, 100, 0.01, gds)
-print("Constant GD")
-print(status)
-print(gds.getX())
-print()
-
-# Adaptive GD
-gds = Tasmanian.Optimization.GradientDescentState(x0, adaptive_stepsize)
-status = Tasmanian.Optimization.AdaptiveGradientDescent(func, grad, 2.0, 2.0, 100, 0.01, gds)
-print("Adaptive GD")
-print(status)
-print(gds.getX())
-print()
-
-# Adaptive PGD
-proj = lambda x : np.array([min(max(0.5, xi), 2.0) for xi in x], np.float64)
-gds = Tasmanian.Optimization.GradientDescentState(x0, adaptive_stepsize)
-status = Tasmanian.Optimization.AdaptiveProjectedGradientDescent(func, grad, proj, 2.0, 2.0, 100, 0.01, gds)
-print("Adaptive PGD")
-print(status)
-print(gds.getX())
-print()
