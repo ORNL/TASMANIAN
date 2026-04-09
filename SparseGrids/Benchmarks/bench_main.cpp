4adaadd2f5dc6701fdca89ffd15b2f4f4683cce7
Miroslav Stoyanov <stoyanovmk@ornl.gov>
2026-04-09 17:44:01 -0400

    switching to large strings

--- SparseGrids/Benchmarks/bench_main.cpp
+++ SparseGrids/Benchmarks/bench_main.cpp
@@ -58,97 +58,119 @@ int main(int argc, const char** argv){
 
 void printHelp(BenchFuction test){
     if (test == bench_none){
-        cout << "\nusage: ./benchmark <function> <parameters>\n\n";
-        cout << "functions: makegrid, loadneeded, evaluate(-mixed), differentiate, iweights, refine\n";
-        cout << "\n see: ./benchmark <function> help\n";
+        cout << R"BENCH(
+usage: ./benchmark <function> <parameters>
+
+functions: makegrid, loadneeded, evaluate(-mixed), differentiate, iweights, refine
+
+  see: ./benchmark <function> help
+)BENCH";
     }else if (test == bench_make){
-        cout << "\nusage: ./benchmark makegrid <grid> <dims> <depth> <type> <rule> <iters> <jumps> <aniso>\n\n";
-        cout << "grid  : global, sequence, localp, wavelet, fourier\n";
-        cout << "dims  : number of dimensions\n";
-        cout << "depth : grid density\n";
-        cout << "type  : level, iptotal, etc.; ignored if not used by the grid\n";
-        cout << "rule  : rleja, clenshaw-curtis, etc.; ignored for wavelet and fourier grids\n";
-        cout << "iters : number of times to repeat the function call\n";
-        cout << "jumps : how many times to increment <depth> by 1\n";
-        cout << "aniso : (optional) list of anisotropic weights and level limits\n";
-        cout << "      : anisotropic weights come first (if used by the grid), then level limits\n";
+        cout << R"BENCH(
+usage: ./benchmark makegrid <grid> <dims> <depth> <type> <rule> <iters> <jumps> <aniso>
+
+grid  : global, sequence, localp, wavelet, fourier
+dims  : number of dimensions
+depth : grid density
+type  : level, iptotal, etc.; ignored if not used by the grid
+rule  : rleja, clenshaw-curtis, etc.; ignored for wavelet and fourier grids
+iters : number of times to repeat the function call
+jumps : how many times to increment <depth> by 1
+aniso : (optional) list of anisotropic weights and level limits
+      : anisotropic weights come first (if used by the grid), then level limits
+)BENCH";
     }else if (test == bench_loadneeded){
-        cout << "\nusage: ./benchmark loadneeded <grid> <dims> <outs> <depth> <type> <rule> <order> <iters> <jumps> <acc> <gpu> <extra>\n\n";
-        cout << "grid  : global, sequence, localp, wavelet, fourier\n";
-        cout << "dims  : number of dimensions\n";
-        cout << "outs  : number of outputs\n";
-        cout << "depth : grid density\n";
-        cout << "type  : level, iptotal, etc.; ignored if not used by the grid\n";
-        cout << "rule  : rleja, clenshaw-curtis, etc.; ignored for wavelet and fourier grids\n";
-        cout << "order : -1, 0, 1, 2; ignored if not used by the grid\n";
-        cout << "iters : number of times to repeat the function call\n";
-        cout << "jumps : how many times to double <outs>\n";
-        cout << "acc   : acceleration type, e.g., gpu-cuda, cpu-blas, none, etc.\n";
-        cout << "gpu   : cuda device ID; ignored for cpu acceleration\n";
-        cout << "extra : (optional) sparse/dense flavor and/or list of anisotropic weights and level limits\n";
-        cout << "      : anisotropic weights come first (if used by the grid), then level limits\n";
+        cout << R"BENCH(
+usage: ./benchmark loadneeded <grid> <dims> <outs> <depth> <type> <rule> <order> <iters> <jumps> <acc> <gpu> <extra>
+
+grid  : global, sequence, localp, wavelet, fourier
+dims  : number of dimensions
+outs  : number of outputs
+depth : grid density
+type  : level, iptotal, etc.; ignored if not used by the grid
+rule  : rleja, clenshaw-curtis, etc.; ignored for wavelet and fourier grids
+order : -1, 0, 1, 2; ignored if not used by the grid
+iters : number of times to repeat the function call
+jumps : how many times to double <outs>
+acc   : acceleration type, e.g., gpu-cuda, cpu-blas, none, etc.
+gpu   : cuda device ID; ignored for cpu acceleration
+extra : (optional) sparse/dense flavor and/or list of anisotropic weights and level limits
+      : anisotropic weights come first (if used by the grid), then level limits
+)BENCH";
     }else if (test == bench_evaluate || test == bench_evaluate_mixed){
-        cout << "\nusage: ./benchmark evaluate <grid> <dims> <outs> <depth> <type> <rule> <order> <batch> <iters> <jumps> <acc> <gpu> <extra>\n\n";
-        cout << "grid  : global, sequence, localp, wavelet, fourier\n";
-        cout << "dims  : number of dimensions\n";
-        cout << "outs  : number of outputs\n";
-        cout << "depth : grid density\n";
-        cout << "type  : level, iptotal, etc.; ignored if not used by the grid\n";
-        cout << "rule  : rleja, clenshaw-curtis, etc.; ignored for wavelet and fourier grids\n";
-        cout << "order : -1, 0, 1, 2; ignored if not used by the grid\n";
-        cout << "batch : number of points to use for the evaluate command\n";
-        cout << "iters : number of times to repeat the function call\n";
-        cout << "jumps : how many times to double <outs>\n";
-        cout << "acc   : acceleration type, e.g., gpu-cuda, cpu-blas, none, etc.\n";
-        cout << "gpu   : cuda device ID; ignored for cpu acceleration\n";
-        cout << "extra : (optional) sparse/dense flavor and/or list of anisotropic weights and level limits\n";
-        cout << "      : anisotropic weights come first (if used by the grid), then level limits\n";
+        cout << R"BENCH(
+usage: ./benchmark evaluate <grid> <dims> <outs> <depth> <type> <rule> <order> <batch> <iters> <jumps> <acc> <gpu> <extra>
+
+grid  : global, sequence, localp, wavelet, fourier
+dims  : number of dimensions
+outs  : number of outputs
+depth : grid density
+type  : level, iptotal, etc.; ignored if not used by the grid
+rule  : rleja, clenshaw-curtis, etc.; ignored for wavelet and fourier grids
+order : -1, 0, 1, 2; ignored if not used by the grid
+batch : number of points to use for the evaluate command
+iters : number of times to repeat the function call
+jumps : how many times to double <outs>
+acc   : acceleration type, e.g., gpu-cuda, cpu-blas, none, etc.
+gpu   : cuda device ID; ignored for cpu acceleration
+extra : (optional) sparse/dense flavor and/or list of anisotropic weights and level limits
+      : anisotropic weights come first (if used by the grid), then level limits
+)BENCH";
     }else if (test == bench_differentiate){
-        cout << "\nusage: ./benchmark differentiate <grid> <dims> <outs> <depth> <type> <rule> <order> <iters> <jumps>\n\n";
-        cout << "grid  : global, sequence, localp, wavelet, fourier\n";
-        cout << "dims  : number of dimensions\n";
-        cout << "outs  : number of outputs\n";
-        cout << "depth : grid density\n";
-        cout << "type  : level, iptotal, etc.; ignored if not used by the grid\n";
-        cout << "rule  : rleja, clenshaw-curtis, etc.; ignored for wavelet and fourier grids\n";
-        cout << "order : -1, 0, 1, 2; ignored if not used by the grid\n";
-        cout << "iters : number of times to repeat the function call\n";
-        cout << "jumps : how many times to double <outs>\n";
-        cout << "extra : (optional) sparse/dense flavor and/or list of anisotropic weights and level limits\n";
-        cout << "      : anisotropic weights come first (if used by the grid), then level limits\n";
+        cout << R"BENCH(
+usage: ./benchmark differentiate <grid> <dims> <outs> <depth> <type> <rule> <order> <iters> <jumps>
+
+grid  : global, sequence, localp, wavelet, fourier
+dims  : number of dimensions
+outs  : number of outputs
+depth : grid density
+type  : level, iptotal, etc.; ignored if not used by the grid
+rule  : rleja, clenshaw-curtis, etc.; ignored for wavelet and fourier grids
+order : -1, 0, 1, 2; ignored if not used by the grid
+iters : number of times to repeat the function call
+jumps : how many times to double <outs>
+extra : (optional) sparse/dense flavor and/or list of anisotropic weights and level limits
+      : anisotropic weights come first (if used by the grid), then level limits
+)BENCH";
     }else if (test == bench_iweights){
-        cout << "\nusage: ./benchmark iweights <grid> <dims> <depth> <type> <rule> <order> <iters> <jumps> <aniso>\n\n";
-        cout << "grid  : global, sequence, localp, wavelet, fourier\n";
-        cout << "dims  : number of dimensions\n";
-        cout << "depth : grid density\n";
-        cout << "type  : level, iptotal, etc.; ignored if not used by the grid\n";
-        cout << "rule  : rleja, clenshaw-curtis, etc.; ignored for wavelet and Fourier grids\n";
-        cout << "order : -1, 0, 1, 2; ignored if not used by the grid\n";
-        cout << "iters : number of times to repeat the function call\n";
-        cout << "jumps : how many times to increase <depth> by 1\n";
-        cout << "aniso : (optional) list of anisotropic weights and level limits\n";
-        cout << "      : anisotropic weights come first (if used by the grid), then level limits\n";
+        cout << R"BENCH(
+usage: ./benchmark iweights <grid> <dims> <depth> <type> <rule> <order> <iters> <jumps> <aniso>
+
+grid  : global, sequence, localp, wavelet, fourier
+dims  : number of dimensions
+depth : grid density
+type  : level, iptotal, etc.; ignored if not used by the grid
+rule  : rleja, clenshaw-curtis, etc.; ignored for wavelet and Fourier grids
+order : -1, 0, 1, 2; ignored if not used by the grid
+iters : number of times to repeat the function call
+jumps : how many times to increase <depth> by 1
+aniso : (optional) list of anisotropic weights and level limits
+      : anisotropic weights come first (if used by the grid), then level limits
+)BENCH";
     }else if (test == bench_refine){
-        cout << "\nusage: ./benchmark refine <grid> <dims> <outs> <depth> <type> <rule> <order> <ref-type-depth> <min-growth> <surp-tolerance> <surp-criteria> <output> <iters> <acc> <gpu> <extra>\n\n";
-        cout << "grid  : global, sequence, localp, wavelet, fourier\n";
-        cout << "dims  : number of dimensions\n";
-        cout << "outs  : number of outputs\n";
-        cout << "depth : grid density\n";
-        cout << "type  : level, iptotal, etc.; ignored if not used by the grid\n";
-        cout << "rule  : rleja, clenshaw-curtis, etc.; ignored for wavelet and fourier grids\n";
-        cout << "order : -1, 0, 1, 2; ignored if not used by the grid\n\n";
+        cout << R"BENCH(
+usage: ./benchmark refine <grid> <dims> <outs> <depth> <type> <rule> <order> <ref-type-depth> <min-growth> <surp-tolerance> <surp-criteria> <output> <iters> <acc> <gpu> <extra>
 
-        cout << "ref-type-depth : (anisotropic refinement) refinement type, e.g., iptotal, ipcurved\n";
-        cout << "min-growth     : (anisotropic refinement) minumum number of refinement points, use 0 to switch to surplus refinement\n";
-        cout << "surp-tolerance : (surplus refinement) tolerance\n";
-        cout << "surp-criteria  : (surplus refinement) selection criteria, e.g., stable, fds\n";
-        cout << "output         : (all refinement) output to use in the refinement\n\n";
+grid  : global, sequence, localp, wavelet, fourier
+dims  : number of dimensions
+outs  : number of outputs
+depth : grid density
+type  : level, iptotal, etc.; ignored if not used by the grid
+rule  : rleja, clenshaw-curtis, etc.; ignored for wavelet and fourier grids
+order : -1, 0, 1, 2; ignored if not used by the grid
 
-        cout << "iters : number of times to repeat the function call\n";
-        cout << "acc   : acceleration type, e.g., gpu-cuda, cpu-blas, none, etc.\n";
-        cout << "gpu   : cuda device ID; ignored for cpu acceleration\n";
-        cout << "extra : (optional) list of anisotropic weights and level limits\n";
-        cout << "      : anisotropic weights come first (if used by the grid), then level limits\n";
+ref-type-depth : (anisotropic refinement) refinement type, e.g., iptotal, ipcurved
+min-growth     : (anisotropic refinement) minumum number of refinement points, use 0 to switch to surplus refinement
+surp-tolerance : (surplus refinement) tolerance
+surp-criteria  : (surplus refinement) selection criteria, e.g., stable, fds
+output         : (all refinement) output to use in the refinement
+
+iters : number of times to repeat the function call
+acc   : acceleration type, e.g., gpu-cuda, cpu-blas, none, etc.
+gpu   : cuda device ID; ignored for cpu acceleration
+extra : (optional) list of anisotropic weights and level limits
+      : anisotropic weights come first (if used by the grid), then level limits
+)BENCH";
     }
     cout << endl;
 }
